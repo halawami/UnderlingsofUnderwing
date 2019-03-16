578f8fbf94ef419049846da5cc185f9a8a334afa
George Main IV <mainge@rose-hulman.edu>
2019-03-16 14:35:05 -0400

    pass testSetup4Players12Rounds

--- Underlings/src/main/java/Game/Game.java
+++ Underlings/src/main/java/Game/Game.java
@@ -7,8 +7,11 @@ public class Game {
 	public void setUp(int numberOfPlayers) {
 		if (numberOfPlayers == 2) {
 			this.roundsLeft = 15;
-		} else {
+		} else if (numberOfPlayers == 3) {
 			this.roundsLeft = 13;
+		}
+		else {
+			this.roundsLeft = 12;
 		}
 		
 	}
