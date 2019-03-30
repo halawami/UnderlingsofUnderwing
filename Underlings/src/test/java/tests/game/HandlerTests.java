b9689916ff6b4ffd440a1bc750d80520634230a0
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-03-30 18:20:00 -0400

    testInitialState pass

--- Underlings/src/test/java/tests/game/HandlerTests.java
+++ Underlings/src/test/java/tests/game/HandlerTests.java
@@ -1,12 +1,18 @@
 package tests.game;
 
-import org.easymock.EasyMock;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import underlings.game.Handler;
+import underlings.game.HandlerState;
 
 public class HandlerTests {
 
 	@Test
-	public void test() {
+	public void testInitialState() {
+		Handler handler = new Handler();
+		assertEquals(HandlerState.READY_ROOM, handler.getState());
 	}
 
 }
