733c52ed5033b7c994c39606329a694547880fec
George Main IV <mainge@rose-hulman.edu>
2019-03-31 09:15:39 -0400

    pass testInitNumberRemaining

--- Underlings/src/main/java/underlings/element/ElementBag.java
+++ Underlings/src/main/java/underlings/element/ElementBag.java
@@ -1,9 +1,31 @@
 package underlings.element;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ElementBag {
 
+	Map<ElementColor, Integer> elementCount;
+	
+	public ElementBag() {
+		this.elementCount = new HashMap<>();
+		this.elementCount.put(ElementColor.BLUE, 20);
+		this.elementCount.put(ElementColor.RED, 20);
+		this.elementCount.put(ElementColor.YELLOW, 20);
+		this.elementCount.put(ElementColor.PURPLE, 10);
+		this.elementCount.put(ElementColor.GREEN, 10);
+		this.elementCount.put(ElementColor.ORANGE, 10);
+		this.elementCount.put(ElementColor.WHITE, 5);
+		this.elementCount.put(ElementColor.BLACK, 5);
+	}
+	
 	public Element drawElementFromList(ElementColor... colors) {
 		return new Element(colors[0]);
 	}
 
+	public int getNumberRemaining(ElementColor color) {
+		return this.elementCount.get(color);
+	}
+
 }
+	
--- Underlings/src/test/java/tests/game/ElementTests.java
+++ Underlings/src/test/java/tests/game/ElementTests.java
@@ -66,4 +66,16 @@ public class ElementTests {
 		assertEquals(ElementColor.BLACK, drawnElement.getColor());
 	}
 	
+	@Test
+	public void testInitNumberRemaining() {
+		assertEquals(20, this.elementBag.getNumberRemaining(ElementColor.BLUE));
+		assertEquals(20, this.elementBag.getNumberRemaining(ElementColor.RED));
+		assertEquals(20, this.elementBag.getNumberRemaining(ElementColor.YELLOW));
+		assertEquals(10, this.elementBag.getNumberRemaining(ElementColor.PURPLE));
+		assertEquals(10, this.elementBag.getNumberRemaining(ElementColor.GREEN));
+		assertEquals(10, this.elementBag.getNumberRemaining(ElementColor.ORANGE));
+		assertEquals(5, this.elementBag.getNumberRemaining(ElementColor.WHITE));
+		assertEquals(5, this.elementBag.getNumberRemaining(ElementColor.BLACK));
+	}
+	
 }
