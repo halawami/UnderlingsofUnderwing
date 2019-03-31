2c703cc61200c2fd3048e52a9e0f1c33c9ab0f35
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-03-31 00:46:43 -0400

    testMoveToIllegateStateFromCard pass

--- Underlings/src/test/java/tests/game/HandlerTests.java
+++ Underlings/src/test/java/tests/game/HandlerTests.java
@@ -14,70 +14,70 @@ public class HandlerTests {
 		Handler handler = new Handler();
 		assertEquals(HandlerState.READY_ROOM, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToUnclaimedAndUnhatchedEggFromReadyRoom(){
+	public void testMoveToUnclaimedAndUnhatchedEggFromReadyRoom() {
 		Handler handler = new Handler();
 		handler.moveToCard();
 		assertEquals(HandlerState.CARD, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToFieldFromReadyRoom(){
+	public void testMoveToFieldFromReadyRoom() {
 		Handler handler = new Handler();
 		handler.moveToField();
 		assertEquals(HandlerState.FIELD, handler.getState());
 	}
 
 	@Test
-	public void testMoveToFieldWhiteFromReadyRoom(){
+	public void testMoveToFieldWhiteFromReadyRoom() {
 		Handler handler = new Handler();
 		handler.moveToFieldWhite();
 		assertEquals(HandlerState.FIELD_WHITESPACE, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToReadyRoomFromReadyRoom(){
+	public void testMoveToReadyRoomFromReadyRoom() {
 		Handler handler = new Handler();
 		handler.moveToReadyRoom();
 		assertEquals(HandlerState.READY_ROOM, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToIllegalStateFromReadyRoom(){
+	public void testMoveToIllegalStateFromReadyRoom() {
 		Handler handler = new Handler();
 		handler.moveToBreakRoom();
 		assertEquals(HandlerState.READY_ROOM, handler.getState());
 		handler.moveToIncubation();
 		assertEquals(HandlerState.READY_ROOM, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveClockwiseInField(){
+	public void testMoveClockwiseInField() {
 		Handler handler = new Handler();
 		handler.moveToField();
 		handler.moveClockwiseInField();
 		assertEquals(HandlerState.FIELD, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToBreakRoomFromField(){
+	public void testMoveToBreakRoomFromField() {
 		Handler handler = new Handler();
 		handler.moveToField();
 		handler.moveToBreakRoom();
 		assertEquals(HandlerState.BREAK_ROOM, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToIllegalStateFromField(){
+	public void testMoveToIllegalStateFromField() {
 		Handler handler = new Handler();
 		handler.moveToField();
 		handler.moveToReadyRoom();
 		assertEquals(HandlerState.FIELD, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToBreakRoomFromCard(){
+	public void testMoveToBreakRoomFromCard() {
 		Handler handler = new Handler();
 		handler.moveToCard();
 		handler.moveToBreakRoom();
@@ -85,31 +85,39 @@ public class HandlerTests {
 	}
 	
 	@Test
-	public void testMoveToCardFromCard(){
+	public void testMoveToIllegateStateFromCard() {
+		Handler handler = new Handler();
+		handler.moveToCard();
+		handler.moveToReadyRoom();
+		assertEquals(HandlerState.CARD, handler.getState());
+	}
+
+	@Test
+	public void testMoveToCardFromCard() {
 		Handler handler = new Handler();
 		handler.moveToCard();
 		handler.moveToCard();
 		assertEquals(HandlerState.CARD, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToReadyRoomFromBreakRoom(){
+	public void testMoveToReadyRoomFromBreakRoom() {
 		Handler handler = new Handler();
 		handler.moveToBreakRoom();
 		handler.moveToReadyRoom();
 		assertEquals(HandlerState.READY_ROOM, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToBreakRoomFromFieldWhiteSpace(){
+	public void testMoveToBreakRoomFromFieldWhiteSpace() {
 		Handler handler = new Handler();
 		handler.moveToFieldWhite();
 		handler.moveToBreakRoom();
 		assertEquals(HandlerState.BREAK_ROOM, handler.getState());
 	}
-	
+
 	@Test
-	public void testMoveToIncubationFromIncubation(){
+	public void testMoveToIncubationFromIncubation() {
 		Handler handler = new Handler(HandlerState.INCUBATION);
 		handler.moveToIncubation();
 		assertEquals(HandlerState.INCUBATION, handler.getState());
