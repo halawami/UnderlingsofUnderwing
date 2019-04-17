c8403ae0318151a8a757e66a59642e2d5b2d5083
Mohammad Baqer <mtbaqer@gmail.com>
2019-04-17 13:27:51 -0400

    refactor handlerStates test

--- Underlings/src/main/java/underlings/game/Handler.java
+++ Underlings/src/main/java/underlings/game/Handler.java
@@ -7,58 +7,59 @@ import java.util.List;
 
 public class Handler {
 
-	private HandlerState state;
-	private final HashMap<HandlerState, List<HandlerState>> allowedStates = initializeHashMap();
+    private HandlerState state;
+    private HashMap<HandlerState, List<HandlerState>> allowedStates;
 
-	public Handler(HandlerState state) {
-		this.state = state;
-	}
+    public Handler(HandlerState state) {
+        this.state = state;
+        this.initializeHashMap();
+    }
 
-	public HashMap<HandlerState, List<HandlerState>> initializeHashMap() {
-		HashMap<HandlerState, List<HandlerState>> toReturn = new HashMap<>();
-		toReturn.put(HandlerState.READY_ROOM, createStateList(
-				HandlerState.CARD, 
-				HandlerState.FIELD,
-				HandlerState.FIELD_WHITESPACE, 
-				HandlerState.READY_ROOM));
-		
-		toReturn.put(HandlerState.FIELD, createStateList(
-				HandlerState.FIELD,
-				HandlerState.BREAK_ROOM));
-		
-		toReturn.put(HandlerState.CARD, createStateList(
-				HandlerState.CARD, 
-				HandlerState.BREAK_ROOM));
-		
-		toReturn.put(HandlerState.BREAK_ROOM, createStateList(HandlerState.READY_ROOM));
-		toReturn.put(HandlerState.FIELD_WHITESPACE, createStateList(HandlerState.BREAK_ROOM));
-		
-		toReturn.put(HandlerState.INCUBATION, createStateList(
-				HandlerState.INCUBATION,
-				HandlerState.READY_ROOM));
-		return toReturn;
-	}
+    private void initializeHashMap() {
+        this.allowedStates = new HashMap<>();
 
-	public List<HandlerState> createStateList(HandlerState... handlerStates) {
-		return new LinkedList<>(Arrays.asList(handlerStates));
-	}
+        this.allowedStates.put(HandlerState.READY_ROOM, createStateList(
+                HandlerState.CARD,
+                HandlerState.FIELD,
+                HandlerState.FIELD_WHITESPACE,
+                HandlerState.READY_ROOM));
 
-	public HandlerState getState() {
-		return this.state;
-	}
+        this.allowedStates.put(HandlerState.FIELD, createStateList(
+                HandlerState.FIELD,
+                HandlerState.BREAK_ROOM));
 
-	public void moveToState(HandlerState state){
-		if (getPossibleStates().contains(state)) {
-			this.state = state;
-		}
-	}
-	
-	public void moveClockwiseInField() {
+        this.allowedStates.put(HandlerState.CARD, createStateList(
+                HandlerState.CARD,
+                HandlerState.BREAK_ROOM));
 
-	}
+        this.allowedStates.put(HandlerState.BREAK_ROOM, createStateList(HandlerState.READY_ROOM));
+        this.allowedStates.put(HandlerState.FIELD_WHITESPACE, createStateList(HandlerState.BREAK_ROOM));
 
-	public List<HandlerState> getPossibleStates() {
-		return allowedStates.get(this.state);
-	}
+        this.allowedStates.put(HandlerState.INCUBATION, createStateList(
+                HandlerState.INCUBATION,
+                HandlerState.READY_ROOM));
+    }
+
+    public List<HandlerState> createStateList(HandlerState... handlerStates) {
+        return new LinkedList<>(Arrays.asList(handlerStates));
+    }
+
+    public HandlerState getState() {
+        return this.state;
+    }
+
+    public void moveToState(HandlerState state) {
+        if (getPossibleStates().contains(state)) {
+            this.state = state;
+        }
+    }
+
+    public void moveClockwiseInField() {
+
+    }
+
+    public List<HandlerState> getPossibleStates() {
+        return allowedStates.get(this.state);
+    }
 
 }
