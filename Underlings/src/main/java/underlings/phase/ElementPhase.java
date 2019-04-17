ea94844532240fb130b808afd51a727fc61ca645
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-04-17 16:16:03 -0400

    fix ElementPhaseTests

--- Underlings/src/main/java/underlings/phase/ElementPhase.java
+++ Underlings/src/main/java/underlings/phase/ElementPhase.java
@@ -1,13 +1,14 @@
 package underlings.phase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import underlings.element.Element;
 import underlings.element.ElementBag;
 import underlings.element.ElementGiver;
+import underlings.game.Handler;
 import underlings.game.HatchingGround;
-import underlings.gui.Choice;
-import underlings.gui.ElementDrawChoice;
+import underlings.gui.DrawChoice;
 import underlings.gui.GUI;
 import underlings.player.Player;
 
@@ -17,25 +18,26 @@ public class ElementPhase implements Phase {
 	public void execute(List<Player> players, GUI gui, ElementBag elementBag, HatchingGround hatchingGround,
 			Runnable displayMethod) {
 
-//		for (Player player : players) {
-//
-//			List<ElementGiver> elementGivers = player.getElementGivers();
-//
-//			while (!elementGivers.isEmpty()) {
-//				ElementGiver chosenElementGiver = gui.promptHandler.promptElementGiver(elementGivers);
-//				List<ElementDrawChoice> possibleElementDrawChoices = chosenElementGiver.getElementDrawChoices();
-//
-//				Choice chosenElementDrawChoice = gui.promptHandler.promptElementDrawChoice(possibleElementDrawChoices);
-//
-//				Element drawnElement = elementBag.drawRandomElement();
-//				player.addElement(drawnElement);
-//
-//				elementGivers.remove(chosenElementGiver);
-//
-//				displayMethod.run();
-//
-//			}
-//		}
+		for (Player player : players) {
+
+			List<ElementGiver> elementGivers = new ArrayList<>();
+			for(Handler handler : player.getHandlers())
+				elementGivers.add(handler.elementGiver);
+
+			while (!elementGivers.isEmpty()) {
+				ElementGiver chosenElementGiver = gui.promptHandler.promptElementGiver(elementGivers);
+
+				DrawChoice chosenDrawChoice = gui.promptHandler.promptElementDrawChoice(chosenElementGiver.drawChoices);
+
+				Element drawnElement = elementBag.drawRandomElement();
+				player.addElement(drawnElement);
+
+				elementGivers.remove(chosenElementGiver);
+
+				displayMethod.run();
+
+			}
+		}
 
 	}
 
