46927a1f23d3abbfb2c40f6f74257625d8eb15f2
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-04-19 12:41:53 -0400

    fail simple getValidAdditions tests

--- Underlings/src/test/java/tests/elementspace/single/ValidAdditionsTests.java
+++ Underlings/src/test/java/tests/elementspace/single/ValidAdditionsTests.java
@@ -1,12 +1,14 @@
 package tests.elementspace.single;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 
+import underlings.element.Element;
 import underlings.element.ElementColor;
 import underlings.element.ElementSpace;
 import underlings.element.utilities.ElementSpaceLogic;
@@ -14,82 +16,66 @@ import underlings.element.utilities.ElementSpaceLogic;
 public class ValidAdditionsTests {
 
 	@Test
-	public void testEmpty() {
+	public void testEmptyRed() {
 		ElementSpace elementSpace = new ElementSpace(ElementColor.RED);
 		List<ElementColor> expected = Arrays.asList(ElementColor.RED);
+		
+		assertEquals(expected, ElementSpaceLogic.getValidAdditions(elementSpace));
+	}
+	
+	@Test
+	public void testEmptyBlue() {
+		ElementSpace elementSpace = new ElementSpace(ElementColor.BLUE);
+		List<ElementColor> expected = Arrays.asList(ElementColor.BLUE);
+		
 		assertEquals(expected, ElementSpaceLogic.getValidAdditions(elementSpace));
 	}
 
-//	@Test
-//	public void testRed() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.RED);
-//		Element element = new Element(ElementColor.RED);
-//
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
-//
-//	@Test
-//	public void testGreen() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.GREEN);
-//		Element element = new Element(ElementColor.GREEN);
-//
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
-//
-//	@Test
-//	public void testBlue() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.BLUE);
-//		Element element = new Element(ElementColor.BLUE);
-//
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
-//
-//	@Test
-//	public void testOrange() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.ORANGE);
-//		Element element = new Element(ElementColor.ORANGE);
-//
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
-//
-//	@Test
-//	public void testYellow() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.YELLOW);
-//		Element element = new Element(ElementColor.YELLOW);
-//
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
-//
-//	@Test
-//	public void testPurple() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.PURPLE);
-//		Element element = new Element(ElementColor.PURPLE);
-//
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
-//
-//	@Test
-//	public void testWhite() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.WHITE);
-//		Element element = new Element(ElementColor.WHITE);
-//
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
-//	
-//	@Test
-//	public void testBlack() {
-//		ElementSpace elementSpace = new ElementSpace(ElementColor.BLACK);
-//		Element element = new Element(ElementColor.BLACK);
-//		
-//		elementSpace.addElements(element);
-//		assertTrue(ElementSpaceLogic.isComplete(elementSpace));
-//	}
+	public void testColor(ElementColor color) {
+		ElementSpace elementSpace = new ElementSpace(color);
+		elementSpace.addElements(new Element(color));
+
+		assertTrue(ElementSpaceLogic.getValidAdditions(elementSpace).isEmpty());
+	}
+
+	@Test
+	public void testRed() {
+		testColor(ElementColor.RED);
+	}
+
+	@Test
+	public void testGreen() {
+		testColor(ElementColor.GREEN);
+	}
+
+	@Test
+	public void testBlue() {
+		testColor(ElementColor.BLUE);
+	}
+
+	@Test
+	public void testOrange() {
+		testColor(ElementColor.ORANGE);
+	}
+
+	@Test
+	public void testYellow() {
+		testColor(ElementColor.YELLOW);
+	}
+
+	@Test
+	public void testPurple() {
+		testColor(ElementColor.PURPLE);
+	}
+
+	@Test
+	public void testWhite() {
+		testColor(ElementColor.WHITE);
+	}
+
+	@Test
+	public void testBlack() {
+		testColor(ElementColor.BLACK);
+	}
 
 }
