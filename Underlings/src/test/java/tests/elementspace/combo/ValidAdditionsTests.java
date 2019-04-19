4782ed50c8daf43e7f8069c3daa610a8ca8f3061
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-04-19 12:45:33 -0400

    fail empty combo getValidAdditions tests

---
+++ Underlings/src/test/java/tests/elementspace/combo/ValidAdditionsTests.java
@@ -0,0 +1,32 @@
+package tests.elementspace.combo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import underlings.element.ElementColor;
+import underlings.element.ElementSpace;
+import underlings.element.utilities.ElementSpaceLogic;
+
+public class ValidAdditionsTests {
+
+	@Test
+	public void testEmptyGreen() {
+		ElementSpace elementSpace = new ElementSpace(ElementColor.GREEN);
+		List<ElementColor> expected = Arrays.asList(ElementColor.GREEN, ElementColor.YELLOW, ElementColor.BLUE);
+		
+		assertEquals(expected, ElementSpaceLogic.getValidAdditions(elementSpace));
+	}
+	
+	@Test
+	public void testEmptyOrange() {
+		ElementSpace elementSpace = new ElementSpace(ElementColor.ORANGE);
+		List<ElementColor> expected = Arrays.asList(ElementColor.ORANGE, ElementColor.RED, ElementColor.YELLOW);
+		
+		assertEquals(expected, ElementSpaceLogic.getValidAdditions(elementSpace));
+	}
+
+}
