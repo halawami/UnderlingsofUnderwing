6298f9f52224d373bc492690513b68e81d47ae3a
Mohammad Baqer <mtbaqer@gmail.com>
2019-04-18 23:11:19 -0400

    refactor Handler

--- Underlings/src/main/java/underlings/game/Handler.java
+++ Underlings/src/main/java/underlings/game/Handler.java
@@ -9,48 +9,10 @@ public class Handler {
 
     public ElementGiver elementGiver;
     private HandlerState state;
-    private HashMap<HandlerState, List<HandlerState>> allowedStates;
 
     public Handler(HandlerState state) {
         this.state = state;
-        this.initializeHashMap();
-
-        Map<HandlerState, String> displayStrings = new HashMap<>();
-        displayStrings.put(HandlerState.READY_ROOM, "in Ready Room");
-        displayStrings.put(HandlerState.BREAK_ROOM, "in Break Room");
-        displayStrings.put(HandlerState.INCUBATION, "in Incubation");
-        displayStrings.put(HandlerState.CARD, "on Card");
-
-        this.elementGiver = new ElementGiver("Handler " + displayStrings.get(state), DrawChoice.RANDOM);
-    }
-
-    private void initializeHashMap() {
-        this.allowedStates = new HashMap<>();
-
-        this.allowedStates.put(HandlerState.READY_ROOM, createStateList(
-                HandlerState.CARD,
-                HandlerState.FIELD,
-                HandlerState.FIELD_WHITESPACE,
-                HandlerState.READY_ROOM));
-
-        this.allowedStates.put(HandlerState.FIELD, createStateList(
-                HandlerState.FIELD,
-                HandlerState.BREAK_ROOM));
-
-        this.allowedStates.put(HandlerState.CARD, createStateList(
-                HandlerState.CARD,
-                HandlerState.BREAK_ROOM));
-
-        this.allowedStates.put(HandlerState.BREAK_ROOM, createStateList(HandlerState.READY_ROOM));
-        this.allowedStates.put(HandlerState.FIELD_WHITESPACE, createStateList(HandlerState.BREAK_ROOM));
-
-        this.allowedStates.put(HandlerState.INCUBATION, createStateList(
-                HandlerState.INCUBATION,
-                HandlerState.READY_ROOM));
-    }
-
-    public List<HandlerState> createStateList(HandlerState... handlerStates) {
-        return new LinkedList<>(Arrays.asList(handlerStates));
+        this.elementGiver = new ElementGiver("Handler " + state.displayString, DrawChoice.RANDOM);
     }
 
     public HandlerState getState() {
@@ -68,7 +30,7 @@ public class Handler {
     }
 
     public List<HandlerState> getPossibleStates() {
-        return allowedStates.get(this.state);
+        return this.state.allowedStates;
     }
     
     @Override
