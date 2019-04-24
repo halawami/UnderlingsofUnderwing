7f8ac370019e75a213ba0a3af53e31c0a2312d74
Mohammad Baqer <mtbaqer@gmail.com>
2019-04-24 17:09:16 -0400

    pass testIncompleteCardOneElementSpace

--- Underlings/src/test/java/tests/card/elementPlacement/isCompleteTests.java
+++ Underlings/src/test/java/tests/card/elementPlacement/isCompleteTests.java
@@ -6,6 +6,7 @@ import underlings.card.Card;
 import underlings.element.ElementSpace;
 import underlings.element.utilities.ElementSpaceLogic;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class isCompleteTests {
@@ -23,6 +24,23 @@ public class isCompleteTests {
         EasyMock.replay(mockedElementSpace, elementSpaceLogic);
 
         assertTrue(elementSpaceLogic.isComplete(completeCard));
+
+        EasyMock.verify(mockedElementSpace, elementSpaceLogic);
+    }
+
+    @Test
+    public void testIncompleteCardOneElementSpace(){
+        Card completeCard = new Card();
+        ElementSpace mockedElementSpace = EasyMock.mock(ElementSpace.class);
+        ElementSpaceLogic elementSpaceLogic = EasyMock.partialMockBuilder(ElementSpaceLogic.class)
+                .addMockedMethod("isComplete", ElementSpace.class).createMock();
+
+        completeCard.elementSpaces = new ElementSpace[]{mockedElementSpace};
+        EasyMock.expect(elementSpaceLogic.isComplete(mockedElementSpace)).andReturn(false);
+
+        EasyMock.replay(mockedElementSpace, elementSpaceLogic);
+
+        assertFalse(elementSpaceLogic.isComplete(completeCard));
 
         EasyMock.verify(mockedElementSpace, elementSpaceLogic);
     }
