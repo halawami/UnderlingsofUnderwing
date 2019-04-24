46d15c44f503988c7280e5127f90c88b54e8fe14
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-04-24 15:29:28 -0400

    GetValidElementSpacesTest.blueElement fails

--- Underlings/src/test/java/tests/elementSpaceLogic/GetValidElementSpacesTest.java
+++ Underlings/src/test/java/tests/elementSpaceLogic/GetValidElementSpacesTest.java
@@ -41,4 +41,18 @@ public class GetValidElementSpacesTest {
 		List<ElementSpace> actual = logic.getPlayableSpaces(testCard, ElementColor.RED);
 		assertEquals(Arrays.asList(space1), actual);
 	}
+	
+	@Test
+	public void blueElement() {
+		ElementSpace space1 = new ElementSpace(ElementColor.RED);
+		ElementSpace space2 = new ElementSpace(ElementColor.BLUE);
+		ElementSpace[] cardSpaces = { space1, space2 };
+
+		Card testCard = new Card();
+		testCard.elementSpaces = cardSpaces;
+		
+		ElementSpaceLogic logic = new ElementSpaceLogic();
+		List<ElementSpace> actual = logic.getPlayableSpaces(testCard, ElementColor.BLUE);
+		assertEquals(Arrays.asList(space2), actual);
+	}
 }
