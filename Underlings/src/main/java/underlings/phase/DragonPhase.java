574c95f30fe32b5765cb30937bb1abb965fc7485
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-04-25 15:50:25 -0400

    setup test for DragonPhase fails

--- Underlings/src/main/java/underlings/phase/DragonPhase.java
+++ Underlings/src/main/java/underlings/phase/DragonPhase.java
@@ -1,25 +1,34 @@
 package underlings.phase;
 
+import java.util.List;
+
+import underlings.element.ElementBag;
+import underlings.field.Field;
+import underlings.game.HatchingGround;
+import underlings.gui.GUI;
 import underlings.player.Player;
 
-public class DragonPhase implements Phase {
+public class DragonPhase extends SequentialPhase {
 
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-
+	public DragonPhase(List<Player> players, GUI gui, ElementBag elementBag, HatchingGround hatchingGround,
+			Runnable displayMethod, Field field) {
+		super(players, gui, elementBag, hatchingGround, displayMethod, field);
 	}
 
+	// pull complete eggs from hatching ground
+	// return elements
+	// re-populate hatching ground
 	@Override
 	public void setup() {
-		// TODO Auto-generated method stub
-
+		
 	}
 
+	// hatch incubated eggs
+	// send the handler to the ready room
+	// run the positive effects
 	@Override
 	public void turn(Player player) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- Underlings/src/test/java/tests/phase/DragonPhaseTests.java
+++ Underlings/src/test/java/tests/phase/DragonPhaseTests.java
@@ -1,14 +1,47 @@
 package tests.phase;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.easymock.EasyMock;
 import org.junit.Test;
 
+import underlings.card.Card;
+import underlings.element.ElementBag;
+import underlings.element.ElementColor;
+import underlings.element.ElementSpace;
+import underlings.game.HatchingGround;
 import underlings.phase.DragonPhase;
 import underlings.phase.Phase;
+import underlings.player.Player;
 
 public class DragonPhaseTests {
 
 	@Test
-	public void basicTest() {
-		Phase phase = new DragonPhase();
+	public void testInit() {
+		Phase phase = new DragonPhase(null, null, null, null, null, null);
+	}
+	
+	@Test
+	public void testSetup() {
+		HatchingGround hatchingGround = EasyMock.mock(HatchingGround.class);
+		ElementBag bag = EasyMock.mock(ElementBag.class);
+		Player player = EasyMock.mock(Player.class);
+		List<Player> players = Arrays.asList(player);
+		
+		Card card = new Card();
+		ElementSpace[] spaces = { new ElementSpace(ElementColor.PURPLE) };
+		card.elementSpaces = spaces;
+		spaces[0].elements = Arrays.asList(ElementColor.BLUE, ElementColor.RED);
+		List<Card> completeEggs = Arrays.asList(card);
+		
+		EasyMock.expect(hatchingGround.pullAndReplaceCompleteEggs()).andReturn(completeEggs);
+		bag.putElement(ElementColor.BLUE);
+		bag.putElement(ElementColor.RED);
+		
+		EasyMock.replay(hatchingGround, bag, player);
+		Phase phase = new DragonPhase(players, null, bag, hatchingGround, null, null);
+		phase.setup();
+		EasyMock.verify(hatchingGround, bag, player);	
 	}
 }
