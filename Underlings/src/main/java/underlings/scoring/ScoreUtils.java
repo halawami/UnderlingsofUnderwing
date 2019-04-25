47e4ce2ff4cb31b1d1675413d6cde4ccd66fc23e
George Main IV <mainge@rose-hulman.edu>
2019-04-25 15:30:17 -0400

    pass perfect balance

---
+++ Underlings/src/main/java/underlings/scoring/ScoreUtils.java
@@ -0,0 +1,22 @@
+package underlings.scoring;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import underlings.card.Card;
+import underlings.player.Player;
+
+public class ScoreUtils {
+
+	public Map<Player, Integer> calculateScores(List<Player> players) {
+		Map<Player, Integer> scores = new HashMap<>();
+		
+		return scores;
+	}
+
+	public int calculateTemperature(List<Card> cards) {
+		return 0;
+	}
+	
+}
