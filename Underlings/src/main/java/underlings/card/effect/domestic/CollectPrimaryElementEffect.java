594c95c8234b5a96e911baeca33276cac3fb5890
George Main IV <mainge@rose-hulman.edu>
2019-04-28 13:41:06 -0400

    added toString to effects

--- Underlings/src/main/java/underlings/card/effect/domestic/CollectPrimaryElementEffect.java
+++ Underlings/src/main/java/underlings/card/effect/domestic/CollectPrimaryElementEffect.java
@@ -16,4 +16,9 @@ public class CollectPrimaryElementEffect extends ElementEffect {
 			player.addElement(element);
 		}
 	}
+	
+	@Override
+	public String toString() {
+		return "Collect a random primary element";
+	}
 }
