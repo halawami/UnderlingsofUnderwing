2b2b4876d4a2bd5857ad0150c0f9ce684135db74
Mohammad Baqer <mtbaqer@gmail.com>
2019-04-28 17:23:56 -0400

    pass testAddElementsToAllAdjacentEggsEffectOneColor

--- Underlings/src/main/java/underlings/card/effect/wild/AddElementsToAllAdjacentEggsEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/AddElementsToAllAdjacentEggsEffect.java
@@ -1,17 +1,13 @@
 package underlings.card.effect.wild;
 
-import com.google.common.collect.Sets;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import underlings.card.Card;
 import underlings.card.effect.HatchingGroundEffect;
 import underlings.element.Element;
 import underlings.element.ElementBag;
 import underlings.element.ElementColor;
 import underlings.element.ElementSpace;
-import underlings.element.NullElement;
 import underlings.element.utilities.ElementSpaceLogic;
 import underlings.game.HatchingGround;
 
@@ -25,30 +21,28 @@ public class AddElementsToAllAdjacentEggsEffect extends HatchingGroundEffect {
     List<Card> adjacentCards = hatchingGround.getAdjacentCards(centerCard);
     for (Card adjacentCard : adjacentCards) {
       List<ElementSpace> playableSpaces = elementSpaceLogic
-          .getPlayableSpaces(Arrays.asList(this.elementColors),
-              adjacentCard);
+          .getPlayableSpaces(Arrays.asList(this.elementColors), adjacentCard);
       for (ElementSpace playableSpace : playableSpaces) {
         List<ElementColor> playableElementColors = elementSpaceLogic
             .getValidAdditions(playableSpace);
-        Set<ElementColor> elementColorsToAdd = this
-            .getIntersection(playableElementColors, this.elementColors);
-        for (ElementColor elementColorToAdd : elementColorsToAdd) {
-          Element element = elementBag.drawElementFromList(elementColorToAdd);
-          if (element != NullElement.getInstance()) {
-            playableSpace.addElements();
+        for (ElementColor playableElementColor : playableElementColors) {
+          if (elementColorsContains(playableElementColor)) {
+            Element element = elementBag.drawElementFromList(playableElementColor);
+            playableSpace.addElements(element);
           }
         }
       }
     }
   }
 
-  private Set<ElementColor> getIntersection(List<ElementColor> playableElementColors,
-      ElementColor[] possibleElementColorsToAdd) {
-    Set<ElementColor> playableElementColorsSet = new HashSet<>(playableElementColors);
-    Set<ElementColor> possibleElementColorsToAddSet = new HashSet<>(
-        Arrays.asList(possibleElementColorsToAdd));
-
-    return Sets.intersection(playableElementColorsSet, possibleElementColorsToAddSet);
-
+  private boolean elementColorsContains(ElementColor elementColor) {
+    for (int i = 0; i < this.elementColors.length; i++) {
+      if (this.elementColors[i] == elementColor) {
+        return true;
+      }
+    }
+    return false;
   }
+
+
 }
