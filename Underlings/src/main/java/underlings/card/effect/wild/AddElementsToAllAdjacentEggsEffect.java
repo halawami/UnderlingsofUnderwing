90f70d28bc2b4470b6eb976fd2032e4ab7d4725a
George Main IV <mainge@rose-hulman.edu>
2019-04-28 20:38:15 -0400

    fix tostring bug

--- Underlings/src/main/java/underlings/card/effect/wild/AddElementsToAllAdjacentEggsEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/AddElementsToAllAdjacentEggsEffect.java
@@ -2,6 +2,7 @@ package underlings.card.effect.wild;
 
 import java.util.Arrays;
 import java.util.List;
+
 import underlings.card.Card;
 import underlings.card.effect.HatchingGroundEffect;
 import underlings.element.Element;
@@ -13,38 +14,39 @@ import underlings.game.HatchingGround;
 
 public class AddElementsToAllAdjacentEggsEffect extends HatchingGroundEffect {
 
-    public ElementColor[] elementColors;
+	public ElementColor[] elementColors;
 
-    @Override
-    protected void apply(Card centerCard, HatchingGround hatchingGround,
-            ElementBag elementBag, ElementSpaceLogic elementSpaceLogic) {
-        List<Card> adjacentCards = hatchingGround.getAdjacentCards(centerCard);
-        for (Card adjacentCard : adjacentCards) {
-            List<ElementSpace> playableSpaces =
-                    elementSpaceLogic.getPlayableSpaces(
-                            Arrays.asList(this.elementColors), adjacentCard);
-            for (ElementSpace playableSpace : playableSpaces) {
-                List<ElementColor> playableElementColors =
-                        elementSpaceLogic.getValidAdditions(playableSpace);
-                for (ElementColor playableElementColor : playableElementColors) {
-                    if (elementColorsContains(playableElementColor)) {
-                        Element element = elementBag
-                                .drawElementFromList(playableElementColor);
-                        playableSpace.addElements(element);
-                    }
-                }
-            }
-        }
-    }
+	@Override
+	protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag,
+			ElementSpaceLogic elementSpaceLogic) {
+		List<Card> adjacentCards = hatchingGround.getAdjacentCards(centerCard);
+		for (Card adjacentCard : adjacentCards) {
+			List<ElementSpace> playableSpaces = elementSpaceLogic.getPlayableSpaces(Arrays.asList(this.elementColors),
+					adjacentCard);
+			for (ElementSpace playableSpace : playableSpaces) {
+				List<ElementColor> playableElementColors = elementSpaceLogic.getValidAdditions(playableSpace);
+				for (ElementColor playableElementColor : playableElementColors) {
+					if (this.elementColorsContains(playableElementColor)) {
+						Element element = elementBag.drawElementFromList(playableElementColor);
+						playableSpace.addElements(element);
+					}
+				}
+			}
+		}
+	}
 
-    private boolean elementColorsContains(ElementColor elementColor) {
-        for (int i = 0; i < this.elementColors.length; i++) {
-            if (this.elementColors[i] == elementColor) {
-                return true;
-            }
-        }
-        return false;
-    }
+	private boolean elementColorsContains(ElementColor elementColor) {
+		for (int i = 0; i < this.elementColors.length; i++) {
+			if (this.elementColors[i] == elementColor) {
+				return true;
+			}
+		}
+		return false;
+	}
 
+	@Override
+	public String toString() {
+		return "Add elements to adjacent cards";
+	}
 
 }
