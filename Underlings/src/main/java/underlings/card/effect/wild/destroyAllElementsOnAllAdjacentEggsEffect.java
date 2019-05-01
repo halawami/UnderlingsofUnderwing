e651ed9cfd4c1b8c6715d02bd1cfbbc63202dc5c
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-01 15:01:03 -0400

    pass testApplyOneElementColor

--- Underlings/src/main/java/underlings/card/effect/wild/destroyAllElementsOnAllAdjacentEggsEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/destroyAllElementsOnAllAdjacentEggsEffect.java
@@ -7,13 +7,18 @@ import underlings.element.ElementColor;
 import underlings.element.utilities.ElementSpaceLogic;
 import underlings.game.HatchingGround;
 
+import java.util.List;
+
 public class destroyAllElementsOnAllAdjacentEggsEffect extends HatchingGroundEffect {
 
     public ElementColor[] elementColors;
 
     @Override
     protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag, ElementSpaceLogic elementSpaceLogic) {
-
+        List<Card> adjacentCards = hatchingGround.getAdjacentCards(centerCard);
+        for (Card adjacentCard : adjacentCards) {
+            destroyAllElementsOfColorOnCard(elementColors[0], adjacentCard);
+        }
     }
 
     public void destroyAllElementsOfColorOnCard(ElementColor elementColorToDestroy, Card card) {
