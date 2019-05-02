ff2d8775f20f5e228a524c520a59a4113fd156e1
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-01 23:50:31 -0400

    fail testOneDestroyableElementsFirst

--- Underlings/src/test/java/tests/elementspace/destroy/DestroyAllElementsOfColorTests.java
+++ Underlings/src/test/java/tests/elementspace/destroy/DestroyAllElementsOfColorTests.java
@@ -11,10 +11,26 @@ public class DestroyAllElementsOfColorTests {
     public void testNoDestroyableElements() {
         ElementSpace elementSpace = new ElementSpace();
         elementSpace.elements.add(ElementColor.BLUE);
+        elementSpace.elements.add(ElementColor.RED);
 
-        elementSpace.destroyAllElementsOfColor(ElementColor.RED);
+        elementSpace.destroyAllElementsOfColor(ElementColor.GREEN);
 
-        Assert.assertEquals(1, elementSpace.elements.size());
+        Assert.assertEquals(2, elementSpace.elements.size());
         Assert.assertEquals(ElementColor.BLUE, elementSpace.elements.get(0));
+        Assert.assertEquals(ElementColor.RED, elementSpace.elements.get(1));
+    }
+
+    @Test
+    public void testOneDestroyableElementsFirst() {
+        ElementSpace elementSpace = new ElementSpace();
+        elementSpace.elements.add(ElementColor.BLUE);
+        elementSpace.elements.add(ElementColor.RED);
+        elementSpace.elements.add(ElementColor.YELLOW);
+
+        elementSpace.destroyAllElementsOfColor(ElementColor.BLUE);
+
+        Assert.assertEquals(2, elementSpace.elements.size());
+        Assert.assertEquals(ElementColor.RED, elementSpace.elements.get(0));
+        Assert.assertEquals(ElementColor.YELLOW, elementSpace.elements.get(1));
     }
 }
