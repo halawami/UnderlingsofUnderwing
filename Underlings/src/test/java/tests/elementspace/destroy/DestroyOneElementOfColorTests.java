c05e50fbf50f37880b5df4ef086b6662c01ae472
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-02 17:03:40 -0400

    pass testNoDestroyableElements

---
+++ Underlings/src/test/java/tests/elementspace/destroy/DestroyOneElementOfColorTests.java
@@ -0,0 +1,22 @@
+package tests.elementspace.destroy;
+
+import org.junit.Assert;
+import org.junit.Test;
+import underlings.element.ElementColor;
+import underlings.element.ElementSpace;
+
+public class DestroyOneElementOfColorTests {
+
+    @Test
+    public void testNoDestroyableElements() {
+        ElementSpace elementSpace = new ElementSpace();
+        elementSpace.elements.add(ElementColor.BLUE);
+        elementSpace.elements.add(ElementColor.RED);
+
+        elementSpace.destroyOneElementOfColor(ElementColor.GREEN);
+
+        Assert.assertEquals(2, elementSpace.elements.size());
+        Assert.assertEquals(ElementColor.BLUE, elementSpace.elements.get(0));
+        Assert.assertEquals(ElementColor.RED, elementSpace.elements.get(1));
+    }
+}
