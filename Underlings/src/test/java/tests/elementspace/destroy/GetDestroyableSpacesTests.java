b9218bee104dbabff5ec4e91fc8f16b8bbe0c2f0
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-01 23:17:50 -0400

    fail testOneDestroyableSpaces

--- Underlings/src/test/java/tests/elementspace/destroy/GetDestroyableSpacesTests.java
+++ Underlings/src/test/java/tests/elementspace/destroy/GetDestroyableSpacesTests.java
@@ -7,21 +7,39 @@ import underlings.element.ElementColor;
 import underlings.element.ElementSpace;
 import underlings.element.utilities.ElementSpaceLogic;
 
-import java.util.Collections;
 import java.util.List;
 
 public class GetDestroyableSpacesTests {
 
     @Test
     public void testNoDestroyableSpaces() {
-        ElementSpace elementSpace = new ElementSpace();
-        elementSpace.elements = Collections.emptyList();
         Card card = new Card();
-        card.elementSpaces = new ElementSpace[]{elementSpace};
+        card.elementSpaces = getElementSpaces(8);
         ElementSpaceLogic elementSpaceLogic = new ElementSpaceLogic();
 
         List<ElementSpace> destroyableSpaces = elementSpaceLogic.getDestroyableSpaces(card, ElementColor.BLUE);
 
         Assert.assertEquals(0, destroyableSpaces.size());
+    }
+
+    @Test
+    public void testOneDestroyableSpaces() {
+        Card card = new Card();
+        card.elementSpaces = getElementSpaces(8);
+        card.elementSpaces[0].elements.add(ElementColor.BLUE);
+        ElementSpaceLogic elementSpaceLogic = new ElementSpaceLogic();
+
+        List<ElementSpace> destroyableSpaces = elementSpaceLogic.getDestroyableSpaces(card, ElementColor.BLUE);
+
+        Assert.assertEquals(1, destroyableSpaces.size());
+        Assert.assertEquals(card.elementSpaces[0], destroyableSpaces.get(0));
+    }
+
+    private ElementSpace[] getElementSpaces(int numberOfSpaces) {
+        ElementSpace[] elementSpaces = new ElementSpace[numberOfSpaces];
+        for (int i = 0; i < numberOfSpaces; i++) {
+            elementSpaces[i] = new ElementSpace();
+        }
+        return elementSpaces;
     }
 }
