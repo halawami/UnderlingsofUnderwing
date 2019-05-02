6db592eb25fb866fdb6b3da98fd150f6038a3816
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-01 23:59:16 -0400

    pas testOneDestroyableElementsMiddle

--- Underlings/src/test/java/tests/elementspace/destroy/DestroyAllElementsOfColorTests.java
+++ Underlings/src/test/java/tests/elementspace/destroy/DestroyAllElementsOfColorTests.java
@@ -33,4 +33,18 @@ public class DestroyAllElementsOfColorTests {
         Assert.assertEquals(ElementColor.RED, elementSpace.elements.get(0));
         Assert.assertEquals(ElementColor.YELLOW, elementSpace.elements.get(1));
     }
+
+    @Test
+    public void testOneDestroyableElementsMiddle() {
+        ElementSpace elementSpace = new ElementSpace();
+        elementSpace.elements.add(ElementColor.BLUE);
+        elementSpace.elements.add(ElementColor.RED);
+        elementSpace.elements.add(ElementColor.YELLOW);
+
+        elementSpace.destroyAllElementsOfColor(ElementColor.RED);
+
+        Assert.assertEquals(2, elementSpace.elements.size());
+        Assert.assertEquals(ElementColor.BLUE, elementSpace.elements.get(0));
+        Assert.assertEquals(ElementColor.YELLOW, elementSpace.elements.get(1));
+    }
 }
