02815f11f78d6dc10ce2f5d2760637dbb6814fad
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-01 22:56:22 -0400

    pass testNoDestroyableSpaces

--- Underlings/src/main/java/underlings/element/utilities/ElementSpaceLogic.java
+++ Underlings/src/main/java/underlings/element/utilities/ElementSpaceLogic.java
@@ -118,6 +118,6 @@ public class ElementSpaceLogic {
     }
 
     public List<ElementSpace> getDestroyableSpaces(Card card, ElementColor elementColor) {
-        return null;
+        return Collections.emptyList();
     }
 }
