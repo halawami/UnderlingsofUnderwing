0ea00bf4506c41bf18fd78c943d554f2c1c091de
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-04 10:27:54 -0400

    pass testOneUnHatchedEggWithMultipleEffects

--- Underlings/src/test/java/underlings/player/EffectTests.java
+++ Underlings/src/test/java/underlings/player/EffectTests.java
@@ -71,4 +71,29 @@ public class EffectTests {
         assertEquals(0, player.unhatchedCards.size());
     }
 
+    @Test
+    public void testOneUnHatchedEggWithMultipleEffects() {
+        Player player = new Player(6, new HandlerFactory(), 0);
+        Card card = EasyMock.mock(Card.class);
+        Handler handler = EasyMock.mock(Handler.class);
+        handler.moveToState(HandlerState.READY_ROOM);
+        card.handler = handler;
+        player.unhatchedCards.add(card);
+        card.domesticEffects = new Effect[2];
+        Effect effect = EasyMock.mock(Effect.class);
+        card.domesticEffects[0] = effect;
+        card.domesticEffects[1] = effect;
+
+        EasyMock.replay(card, handler, effect);
+        List<Effect> effects = player.getAllEffects();
+
+        EasyMock.verify(card, handler, effect);
+        assertEquals(effect, effects.get(0));
+        assertEquals(effect, effects.get(1));
+        assertEquals(2, effects.size());
+        assertEquals(1, player.hatchedCards.size());
+        assertEquals(card, player.hatchedCards.get(0));
+        assertEquals(0, player.unhatchedCards.size());
+    }
+
 }
