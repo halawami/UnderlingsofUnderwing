b15f5185b01e017af03f85bbc9a4b3ef8de61207
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-04 10:35:41 -0400

    pass testOneCompletedEggs

--- Underlings/src/test/java/underlings/player/CardTests.java
+++ Underlings/src/test/java/underlings/player/CardTests.java
@@ -1,5 +1,6 @@
 package underlings.player;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -9,6 +10,7 @@ import org.junit.Test;
 import underlings.card.Card;
 import underlings.handler.Handler;
 import underlings.handler.HandlerFactory;
+import underlings.handler.HandlerState;
 
 public class CardTests {
     @Test
@@ -29,6 +31,12 @@ public class CardTests {
         Player player = new Player(6, new HandlerFactory(), 0);
         Card card = EasyMock.mock(Card.class);
         card.handler = player.getHandlers().get(0);
+        card.handler.moveToState(HandlerState.INCUBATION);
+        EasyMock.replay(card);
+
+        EasyMock.verify(card);
         assertTrue(player.hasCard(card));
+        assertEquals(1, player.unhatchedCards.size());
+        assertEquals(card, player.unhatchedCards.get(0));
     }
 }
