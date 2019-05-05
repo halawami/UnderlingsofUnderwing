10dfd78e1267c67367c3334eb55e932ae631488c
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-05 16:04:50 -0400

    pass testSixHandlersNoEffectElementGivers

--- Underlings/src/test/java/underlings/player/GetElementGiversTests.java
+++ Underlings/src/test/java/underlings/player/GetElementGiversTests.java
@@ -35,5 +35,29 @@ public class GetElementGiversTests {
     }
 
 
+    @Test
+    public void testSixHandlersNoEffectElementGivers() {
+        HandlerFactory mockHandlerFactory = EasyMock.mock(HandlerFactory.class);
+        Handler handler = new Handler(HandlerState.BREAK_ROOM);
+        handler.elementGiver = EasyMock.mock(ElementGiver.class);
+
+        EasyMock.expect(mockHandlerFactory.createHandler()).andReturn(handler).times(6);
+
+        EasyMock.replay(mockHandlerFactory, handler.elementGiver);
+
+        Player testedPlayer = new Player(6, mockHandlerFactory, 0);
+        for (int i = 0; i < 4; i++) {
+            testedPlayer.gainHandler();
+        }
+
+        List<ElementGiver> elementGivers = testedPlayer.getElementGivers();
+
+        Assert.assertEquals(6, elementGivers.size());
+        for (ElementGiver elementGiver : elementGivers) {
+            Assert.assertEquals(handler.elementGiver, elementGiver);
+        }
+
+        EasyMock.verify(mockHandlerFactory, handler.elementGiver);
+    }
 
 }
