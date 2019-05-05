36fd99a2adb5461b3cee67a9a0307e88a6cad8dc
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-05 16:04:26 -0400

    pass testTwoHandlersNoEffectElementGivers

---
+++ Underlings/src/test/java/underlings/player/GetElementGiversTests.java
@@ -0,0 +1,39 @@
+package underlings.player;
+
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Test;
+
+import underlings.element.ElementGiver;
+import underlings.handler.Handler;
+import underlings.handler.HandlerFactory;
+import underlings.handler.HandlerState;
+
+public class GetElementGiversTests {
+
+    @Test
+    public void testTwoHandlersNoEffectElementGivers() {
+        HandlerFactory mockHandlerFactory = EasyMock.mock(HandlerFactory.class);
+        Handler handler = new Handler(HandlerState.BREAK_ROOM);
+        handler.elementGiver = EasyMock.mock(ElementGiver.class);
+
+        EasyMock.expect(mockHandlerFactory.createHandler()).andReturn(handler).times(2);
+
+        EasyMock.replay(mockHandlerFactory, handler.elementGiver);
+
+        Player testedPlayer = new Player(6, mockHandlerFactory, 0);
+        List<ElementGiver> elementGivers = testedPlayer.getElementGivers();
+
+        Assert.assertEquals(2, elementGivers.size());
+        for (ElementGiver elementGiver : elementGivers) {
+            Assert.assertEquals(handler.elementGiver, elementGiver);
+        }
+
+        EasyMock.verify(mockHandlerFactory, handler.elementGiver);
+    }
+
+
+
+}
