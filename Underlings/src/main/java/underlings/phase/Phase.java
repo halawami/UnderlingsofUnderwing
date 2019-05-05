94ce6e194b5feb0d9e5beea59a3d9c7ec65139d7
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-05 15:32:10 -0400

    change Phase to an abstract class

--- Underlings/src/main/java/underlings/phase/Phase.java
+++ Underlings/src/main/java/underlings/phase/Phase.java
@@ -1,13 +1,38 @@
 package underlings.phase;
 
+import java.util.List;
+
+import underlings.element.ElementBag;
+import underlings.field.Field;
+import underlings.game.HatchingGround;
+import underlings.gui.Gui;
 import underlings.player.Player;
 
-public interface Phase {
+public abstract class Phase {
 
-    public void execute(int turnLeader);
+	protected List<Player> players;
+	public Gui gui;
+	protected ElementBag elementBag;
+	protected HatchingGround hatchingGround;
+	protected Runnable displayMethod;
+	protected Field field;
 
-    public void setup();
+	protected boolean phaseComplete;
 
-    public boolean turn(Player player);
+	public Phase(List<Player> players, Gui gui, ElementBag elementBag, HatchingGround hatchingGround,
+			Runnable displayMethod, Field field) {
+		this.players = players;
+		this.gui = gui;
+		this.elementBag = elementBag;
+		this.hatchingGround = hatchingGround;
+		this.displayMethod = displayMethod;
+		this.field = field;
+	}
+
+	public abstract void execute(int turnLeader);
+
+	public abstract void setup();
+
+	public abstract boolean turn(Player player);
 
 }
