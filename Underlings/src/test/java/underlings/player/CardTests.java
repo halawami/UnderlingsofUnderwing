17945304b53db94505c49705716d3ff777aa8806
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-05 23:44:34 -0400

    remove unnecessary test from player.CardTests

--- Underlings/src/test/java/underlings/player/CardTests.java
+++ Underlings/src/test/java/underlings/player/CardTests.java
@@ -13,18 +13,6 @@ import underlings.handler.HandlerFactory;
 import underlings.handler.HandlerState;
 
 public class CardTests {
-    @Test
-    public void testNoCompletedEggs() {
-        Card card = EasyMock.mock(Card.class);
-        Handler handler = EasyMock.mock(Handler.class);
-        card.handler = handler;
-
-        EasyMock.replay(card, handler);
-
-        EasyMock.verify(card, handler);
-        Player player = new Player(6, new HandlerFactory(), 0);
-        assertFalse(player.hasCard(card));
-    }
 
     @Test
     public void testOneCompletedEggs() {
@@ -32,7 +20,7 @@ public class CardTests {
         Card card = new Card();
         card.handler = player.getHandlers().get(0);
         card.handler.moveToState(HandlerState.INCUBATION);
-        
+
         assertTrue(player.hasCard(card));
         assertEquals(1, player.unhatchedCards.size());
         assertEquals(card, player.unhatchedCards.get(0));
