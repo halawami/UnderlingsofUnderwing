4b16c47303ed8ce40888f44a7c91f79f1f703a46
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-05 21:26:53 -0400

    add coverage for drawing an element from an empty bag

---
+++ Underlings/src/test/java/underlings/element/ElementBagTests.java
@@ -0,0 +1,35 @@
+package underlings.element;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class ElementBagTests {
+
+    @Test
+    public void testEmptyBag()
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        ElementBag bag = new ElementBag(new ElementFactory(), new Random());
+        Field privateMap = ElementBag.class.getDeclaredField("elementCount");
+        privateMap.setAccessible(true);
+
+        @SuppressWarnings("unchecked")
+        Map<ElementColor, Integer> elementCounts = (Map<ElementColor, Integer>) privateMap.get(bag);;
+        elementCounts.put(ElementColor.BLUE, 0);
+        elementCounts.put(ElementColor.RED, 0);
+        elementCounts.put(ElementColor.YELLOW, 0);
+        elementCounts.put(ElementColor.PURPLE, 0);
+        elementCounts.put(ElementColor.GREEN, 0);
+        elementCounts.put(ElementColor.ORANGE, 0);
+        elementCounts.put(ElementColor.WHITE, 0);
+        elementCounts.put(ElementColor.BLACK, 0);
+
+        Element actual = bag.drawRandomElement();
+        Element expected = NullElement.getInstance();
+        assertEquals(expected, actual);
+    }
+}
