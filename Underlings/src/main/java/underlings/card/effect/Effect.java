4bda3cb4624b84b1692bf1a3c009188f95e363c4
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-07 01:03:55 -0400

    add gui parameter to apply

--- Underlings/src/main/java/underlings/card/effect/Effect.java
+++ Underlings/src/main/java/underlings/card/effect/Effect.java
@@ -4,6 +4,7 @@ import underlings.card.Card;
 import underlings.element.ElementBag;
 import underlings.element.utilities.ElementSpaceLogic;
 import underlings.game.HatchingGround;
+import underlings.gui.Gui;
 import underlings.player.Player;
 
 public abstract class Effect {
@@ -47,13 +48,13 @@ public abstract class Effect {
     }
 
     protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag,
-            ElementSpaceLogic elementSpaceLogic) {
-    }
+            ElementSpaceLogic elementSpaceLogic) {}
 
-    protected void apply(Player player, ElementBag elementBag) {
-    }
+    protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag,
+            ElementSpaceLogic elementSpaceLogic, Gui gui) {}
 
-    protected void apply(Player player) {
-    }
-    
+    protected void apply(Player player, ElementBag elementBag) {}
+
+    protected void apply(Player player) {}
+
 }
--- Underlings/src/main/java/underlings/card/effect/wild/HatchAllUnclaimedAdjacentEggsEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/HatchAllUnclaimedAdjacentEggsEffect.java
@@ -1,11 +1,16 @@
 package underlings.card.effect.wild;
 
+import java.util.List;
+
 import underlings.card.Card;
 import underlings.card.Family;
 import underlings.card.effect.HatchingGroundEffect;
 import underlings.element.ElementBag;
 import underlings.element.utilities.ElementSpaceLogic;
 import underlings.game.HatchingGround;
+import underlings.gui.Gui;
+import underlings.player.FakePlayer;
+import underlings.utilities.WildEggHatchingLogic;
 
 public class HatchAllUnclaimedAdjacentEggsEffect extends HatchingGroundEffect {
 
@@ -13,8 +18,14 @@ public class HatchAllUnclaimedAdjacentEggsEffect extends HatchingGroundEffect {
 
     @Override
     protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag,
-            ElementSpaceLogic elementSpaceLogic) {
-
+            ElementSpaceLogic elementSpaceLogic, Gui gui) {
+        List<Card> adjacentCards = hatchingGround.getAdjacentCards(centerCard);
+        WildEggHatchingLogic wildEggHatchingLogic = new WildEggHatchingLogic(gui, elementBag, hatchingGround, FakePlayer.getInstance());
+        for (Card adjacentCard : adjacentCards) {
+            if (){
+                
+            }
+        }
     }
 
 }
