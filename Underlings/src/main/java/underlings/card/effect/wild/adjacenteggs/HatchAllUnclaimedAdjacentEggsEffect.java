9a3fcf6fb398c760941ea4c654606094700bf002
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-07 20:18:57 -0400

    refactor HatchAllUnclaimedAdjacentEggsEffect to extend AdjacentEggsEffect

--- Underlings/src/main/java/underlings/card/effect/wild/adjacenteggs/HatchAllUnclaimedAdjacentEggsEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/adjacenteggs/HatchAllUnclaimedAdjacentEggsEffect.java
@@ -1,28 +1,17 @@
 package underlings.card.effect.wild.adjacenteggs;
 
-import java.util.List;
-
 import underlings.card.Card;
 import underlings.card.Family;
-import underlings.card.effect.HatchingGroundEffect;
 import underlings.element.ElementBag;
 import underlings.element.utilities.ElementSpaceLogic;
-import underlings.game.HatchingGround;
-import underlings.gui.Gui;
-import underlings.player.FakePlayer;
-import underlings.utilities.WildEggHatchingLogic;
 
-public class HatchAllUnclaimedAdjacentEggsEffect extends HatchingGroundEffect {
+public class HatchAllUnclaimedAdjacentEggsEffect extends AdjacentEggsEffect {
 
     public Family[] dragonFamilies;
 
     @Override
-    protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag,
-            ElementSpaceLogic elementSpaceLogic, Gui gui) {
-        List<Card> adjacentCards = hatchingGround.getAdjacentCards(centerCard);
-        WildEggHatchingLogic wildEggHatchingLogic =
-                new WildEggHatchingLogic(gui, elementBag, hatchingGround, FakePlayer.getInstance());
-        wildEggHatchingLogic.hatchWildEgg(adjacentCards.get(0));
+    protected void applyOnAdjacentEgg(Card adjacentEgg, ElementBag elementBag, ElementSpaceLogic elementSpaceLogic) {
+
     }
 
 }
