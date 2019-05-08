baa20c015fe6960b9078137a5e8ea3ebfbf10b47
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-08 05:17:02 -0400

    pass testNoSelectableElement

--- Underlings/src/test/java/underlings/effect/hatchingground/CollectOneElementFromAnyEggInPlayEffectTests.java
+++ Underlings/src/test/java/underlings/effect/hatchingground/CollectOneElementFromAnyEggInPlayEffectTests.java
@@ -11,11 +11,39 @@ import underlings.card.effect.domestic.CollectOneElementFromAnyEggInPlayEffect;
 import underlings.element.Element;
 import underlings.element.ElementColor;
 import underlings.element.ElementSpace;
+import underlings.element.NullElement;
 import underlings.game.HatchingGround;
 import underlings.gui.Gui;
 import underlings.player.Player;
 
 public class CollectOneElementFromAnyEggInPlayEffectTests {
+
+    @Test
+    public void testNoSelectableElement() {
+        Player currentPlayer = EasyMock.mock(Player.class);
+        HatchingGround hatchingGround = EasyMock.mock(HatchingGround.class);
+        List<Card> mockedCards = this.getMockedCards(6);
+        Gui gui = EasyMock.mock(Gui.class);
+        ElementSpace elementSpace = EasyMock.mock(ElementSpace.class);
+        CollectOneElementFromAnyEggInPlayEffect testedEffect = new CollectOneElementFromAnyEggInPlayEffect();
+        testedEffect.elementChoices = new ElementColor[]{ElementColor.BLUE};
+        testedEffect.on(gui).on(currentPlayer).on(hatchingGround);
+
+        EasyMock.expect(hatchingGround.getAllCards()).andReturn(mockedCards);
+        EasyMock.expect(
+                gui.getElementSpaceContainingElementOfColors(mockedCards, testedEffect.elementChoices))
+                .andReturn(elementSpace);
+        EasyMock.expect(gui.getElementOfColorsFromSpace(testedEffect.elementChoices, elementSpace))
+                .andReturn(NullElement.getInstance());
+        elementSpace.destroyOneElementOfColor(ElementColor.NULL);
+
+        EasyMock.replay(currentPlayer, hatchingGround, gui, elementSpace);
+
+        testedEffect.apply();
+
+        EasyMock.verify(currentPlayer, hatchingGround, gui, elementSpace);
+
+    }
 
     @Test
     public void testASelectableElement() {
@@ -45,6 +73,7 @@ public class CollectOneElementFromAnyEggInPlayEffectTests {
         EasyMock.verify(currentPlayer, hatchingGround, gui, elementSpace, element);
 
     }
+
 
     private List<Card> getMockedCards(int numberOfCards) {
         List<Card> mockedCards = new ArrayList<>();
