97af06d7027b144deb39e3433d122aa18880a2ff
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-07 21:01:56 -0400

    pass testUnhatchableAdjacentEgg

--- Underlings/src/main/java/underlings/card/effect/wild/adjacenteggs/HatchAllUnclaimedAdjacentEggsEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/adjacenteggs/HatchAllUnclaimedAdjacentEggsEffect.java
@@ -11,7 +11,7 @@ public class HatchAllUnclaimedAdjacentEggsEffect extends AdjacentEggsEffect {
 
     @Override
     public void applyOnAdjacentEgg(Card adjacentEgg, ElementBag elementBag, ElementSpaceLogic elementSpaceLogic) {
-
+        this.isHatchableEgg(adjacentEgg);
     }
 
     public boolean isHatchableEgg(Card adjacentEgg) {
