bfe5bf6606d1d7cf1ce5a836014fc69a32148eae
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-09 02:29:13 -0400

    fail testAttemptHatchClaimedEgg

--- Underlings/src/test/java/underlings/effect/hatchingground/HatchAllUnclaimedAdjacentEggsEffectTests.java
+++ Underlings/src/test/java/underlings/effect/hatchingground/HatchAllUnclaimedAdjacentEggsEffectTests.java
@@ -14,6 +14,7 @@ import underlings.card.effect.wild.adjacenteggs.HatchAllUnclaimedEffect;
 import underlings.element.ElementBag;
 import underlings.game.HatchingGround;
 import underlings.gui.Gui;
+import underlings.handler.Handler;
 import underlings.handler.WildHandler;
 import underlings.player.FakePlayer;
 import underlings.player.Player;
@@ -48,6 +49,28 @@ public class HatchAllUnclaimedAdjacentEggsEffectTests {
         hatchAllUnclaimedAdjacentEggsEffect.applyOnAdjacentEgg(mockedCards.get(0), elementBag,
                 fakePlayer.elementSpaceLogic, hatchingGround, gui);
         assertEquals(mockedCards.get(0).handler, WildHandler.getInstance());
+        EasyMock.verify(hatchingGround, elementBag, effect, gui);
+    }
+
+    @Test
+    public void testAttemptHatchClaimedEgg() {
+        Card centerCard = new Card();
+        List<Card> mockedCards = getMockedCards(1);
+        mockedCards.get(0).wildEffects = new Effect[1];
+        Effect effect = EasyMock.mock(Effect.class);
+        mockedCards.get(0).wildEffects[0] = effect;
+        mockedCards.get(0).handler = EasyMock.mock(Handler.class);
+        HatchingGround hatchingGround = EasyMock.mock(HatchingGround.class);
+
+        ElementBag elementBag = EasyMock.mock(ElementBag.class);
+        Player fakePlayer = FakePlayer.getInstance();
+        Gui gui = EasyMock.mock(Gui.class);
+        EasyMock.replay(hatchingGround, elementBag, effect, gui);
+
+        HatchAllUnclaimedEffect hatchAllUnclaimedAdjacentEggsEffect = new HatchAllUnclaimedEffect();
+
+        hatchAllUnclaimedAdjacentEggsEffect.applyOnAdjacentEgg(mockedCards.get(0), elementBag,
+                fakePlayer.elementSpaceLogic, hatchingGround, gui);
         EasyMock.verify(hatchingGround, elementBag, effect, gui);
     }
 
