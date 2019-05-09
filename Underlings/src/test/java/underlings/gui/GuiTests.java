3fad6737bf41f03ce40a97c0e5eb83cfbf1c32fd
George Main IV <mainge@rose-hulman.edu>
2019-05-08 22:07:02 -0400

    refactor GuiTests

--- Underlings/src/test/java/underlings/gui/GuiTests.java
+++ Underlings/src/test/java/underlings/gui/GuiTests.java
@@ -51,88 +51,59 @@ public class GuiTests {
 
 	@Test
 	public void testGetPlayerCountTwoPlayers() {
-
-		// RECORD
 		EasyMock.expect(this.promptHandler.promptInt("Enter Player Count", 2, 6)).andReturn(2);
 
-		// REPLAY
 		EasyMock.replay(this.promptHandler, this.display);
 		this.game.promptPlayerCount();
 
-		// VERIFY
-		EasyMock.verify(this.promptHandler, this.display);
 		assertEquals(2, this.game.getPlayerCount());
 
 	}
 
 	@Test
 	public void testGetPlayerCountSixPlayers() {
-
-		// RECORD
 		EasyMock.expect(this.promptHandler.promptInt("Enter Player Count", 2, 6)).andReturn(6);
 
-		// REPLAY
 		EasyMock.replay(this.promptHandler, this.display);
 		this.game.promptPlayerCount();
 
-		// VERIFY
-		EasyMock.verify(this.promptHandler, this.display);
 		assertEquals(6, this.game.getPlayerCount());
-
 	}
 
 	@Test
 	public void testDisplayCardSetupTwoPlayers() {
 		this.game.setUp(2);
 
-		// RECORD
 		EasyMock.expect(this.promptHandler.promptInt("Enter Player Count [2, 6]", 2, 6)).andStubReturn(2);
 		this.display.displayCard(EasyMock.anyInt(), EasyMock.anyInt(), EasyMock.anyObject(Card.class));
 		EasyMock.expectLastCall().times(6);
 
-		// REPLAY
 		EasyMock.replay(this.promptHandler, this.display);
 		this.hatchingGround.display(this.gui);
-
-		// VERIFY
-		EasyMock.verify(this.promptHandler, this.display);
-
 	}
 
 	@Test
 	public void testDisplayCardSetupThreePlayers() {
 		this.game.setUp(3);
 
-		// RECORD
 		EasyMock.expect(this.promptHandler.promptInt("Enter Player Count [2, 6]", 2, 6)).andStubReturn(2);
 		this.display.displayCard(EasyMock.anyInt(), EasyMock.anyInt(), EasyMock.anyObject(Card.class));
 		EasyMock.expectLastCall().times(12);
 
-		// REPLAY
 		EasyMock.replay(this.promptHandler, this.display);
 		this.hatchingGround.display(this.gui);
-
-		// VERIFY
-		EasyMock.verify(this.promptHandler, this.display);
-
 	}
 
 	@Test
 	public void testDisplayCardSetupFourPlayers() {
 		this.game.setUp(4);
 
-		// RECORD
 		EasyMock.expect(this.promptHandler.promptInt("Enter Player Count [2, 6]", 2, 6)).andStubReturn(4);
 		this.display.displayCard(EasyMock.anyInt(), EasyMock.anyInt(), EasyMock.anyObject(Card.class));
 		EasyMock.expectLastCall().times(16);
 
-		// REPLAY
 		EasyMock.replay(this.promptHandler, this.display);
 		this.hatchingGround.display(this.gui);
-
-		// VERIFY
-		EasyMock.verify(this.promptHandler, this.display);
-
 	}
 
 	@Test
