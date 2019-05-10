46c5a09e368766a2ec5796bfe51133204f22b9e0
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-10 11:56:03 -0400

    clean up getEggToHatch method in Gui

--- Underlings/src/main/java/underlings/gui/Gui.java
+++ Underlings/src/main/java/underlings/gui/Gui.java
@@ -21,95 +21,100 @@ import underlings.utilities.LocaleWrap;
 
 public class Gui {
 
-    public PromptHandler promptHandler;
-    public Display display;
+	public PromptHandler promptHandler;
+	public Display display;
 
-    public Gui(PromptHandler promptHandler, Display display) {
-        this.promptHandler = promptHandler;
-        this.display = display;
-    }
+	public Gui(PromptHandler promptHandler, Display display) {
+		this.promptHandler = promptHandler;
+		this.display = display;
+	}
 
-    public DrawChoice getDrawChoice(List<ElementGiver> elementGivers, int playerId) {
-        ElementGiver elementGiver = this.promptHandler.promptChoice("Choose an Element Giver", elementGivers, playerId);
-        elementGivers.remove(elementGiver);
-        return this.promptHandler.promptChoice("Choose a Draw Choice", elementGiver.drawChoices, playerId);
-    }
+	public DrawChoice getDrawChoice(List<ElementGiver> elementGivers, int playerId) {
+		ElementGiver elementGiver = this.promptHandler.promptChoice("Choose an Element Giver", elementGivers, playerId);
+		elementGivers.remove(elementGiver);
+		return this.promptHandler.promptChoice("Choose a Draw Choice", elementGiver.drawChoices, playerId);
+	}
 
-    public boolean getMoreMovesDecision(int choiceNum, int playerId) {
-        if (choiceNum == 0) {
-            return false;
-        }
+	public boolean getMoreMovesDecision(int choiceNum, int playerId) {
+		if (choiceNum == 0) {
+			return false;
+		}
 
-        return this.promptHandler.promptDecision("Would you like to place another element?", playerId);
-    }
+		return this.promptHandler.promptDecision("Would you like to place another element?", playerId);
+	}
 
-    public HandlerDecision getHandlerDecision(List<Handler> handlers, int playerId, HatchingGround hatchingGround) {
-        Handler handler = this.promptHandler.promptChoice("Choose a Handler", handlers, playerId);
-        handlers.remove(handler);
+	public HandlerDecision getHandlerDecision(List<Handler> handlers, int playerId, HatchingGround hatchingGround) {
+		Handler handler = this.promptHandler.promptChoice("Choose a Handler", handlers, playerId);
+		handlers.remove(handler);
 
-        List<HandlerChoice> possibleChoices = handler.getPossibleChoices();
-        possibleChoices = new ArrayList<>(possibleChoices);
-        if (hatchingGround.getUnclaimedEggs().isEmpty()) {
-            possibleChoices.remove(HandlerChoice.CARD);
-        }
+		List<HandlerChoice> possibleChoices = handler.getPossibleChoices();
+		possibleChoices = new ArrayList<>(possibleChoices);
+		if (hatchingGround.getUnclaimedEggs().isEmpty()) {
+			possibleChoices.remove(HandlerChoice.CARD);
+		}
 
-        HandlerChoice handlerChoice =
-                this.promptHandler.promptChoice("Choose a movement for " + handler, possibleChoices, playerId);
+		HandlerChoice handlerChoice = this.promptHandler.promptChoice("Choose a movement for " + handler,
+				possibleChoices, playerId);
 
-        return new HandlerDecision(handler, handlerChoice);
-    }
+		return new HandlerDecision(handler, handlerChoice);
+	}
 
-    public Card getCard(int playerId, HatchingGround hatchingGround, Function<Card, Boolean> isValid) {
-        Card[][] cards = new Card[hatchingGround.getHeight()][hatchingGround.getWidth()];
-        for (int w = 0; w < cards.length; w++) {
-            for (int h = 0; h < cards[w].length; h++) {
-                if (isValid.apply(hatchingGround.cards[w][h])) {
-                    cards[w][h] = hatchingGround.cards[w][h];
-                }
-            }
-        }
+	public Card getCard(int playerId, HatchingGround hatchingGround, Function<Card, Boolean> isValid) {
+		Card[][] cards = new Card[hatchingGround.getHeight()][hatchingGround.getWidth()];
+		for (int w = 0; w < cards.length; w++) {
+			for (int h = 0; h < cards[w].length; h++) {
+				if (isValid.apply(hatchingGround.cards[w][h])) {
+					cards[w][h] = hatchingGround.cards[w][h];
+				}
+			}
+		}
 
-        Card card = this.promptHandler.pickCard("Choose a card", cards, playerId);
-        return card;
-    }
+		Card card = this.promptHandler.pickCard("Choose a card", cards, playerId);
+		return card;
+	}
 
-    public void notifyAction(int playerId, String message) {
-        this.promptHandler.displayMessage(message, playerId, JOptionPane.PLAIN_MESSAGE);
-    }
+	public void notifyAction(int playerId, String message) {
+		this.promptHandler.displayMessage(message, playerId, JOptionPane.PLAIN_MESSAGE);
+	}
 
-    public int getFieldSpace() {
-        return this.promptHandler.promptInt("Enter Field Space", 0, 21);
-    }
+	public int getFieldSpace() {
+		return this.promptHandler.promptInt("Enter Field Space", 0, 21);
+	}
 
-    public int getPlayerCount(int minPlayers, int maxPlayers) {
-        return this.promptHandler.promptInt(LocaleWrap.get("prompt_player_count"), minPlayers, maxPlayers);
-    }
+	public int getPlayerCount(int minPlayers, int maxPlayers) {
+		return this.promptHandler.promptInt(LocaleWrap.get("prompt_player_count"), minPlayers, maxPlayers);
+	}
 
-    public ElementSpace getElementSpaceContainingElementOfColors(List<Card> cards, ElementColor[] colorChoices) {
-        // TODO: implement this method for CollectUpToTwoElementsFromAnyEggInPlayEffect,
-        // ask Mohammad for information
-        return null;
-    }
+	public ElementSpace getElementSpaceContainingElementOfColors(List<Card> cards, ElementColor[] colorChoices) {
+		// TODO: implement this method for CollectUpToTwoElementsFromAnyEggInPlayEffect,
+		// ask Mohammad for information
+		return null;
+	}
 
-    public Element getElementOfColorsFromSpace(ElementColor[] elementChoices, ElementSpace elementSpace) {
-        // TODO: implement this method for CollectUpToTwoElementsFromAnyEggInPlayEffect,
-        // ask Mohammad for information
-        return null;
-    }
+	public Element getElementOfColorsFromSpace(ElementColor[] elementChoices, ElementSpace elementSpace) {
+		// TODO: implement this method for CollectUpToTwoElementsFromAnyEggInPlayEffect,
+		// ask Mohammad for information
+		return null;
+	}
 
-    public Card getEggToHatch(Card[][] cards, int maxPoints, Player player) {
-        Card[][] validCards = new Card[cards.length][cards[0].length];
-        int count = 0;
-        for (int row = 0; row < cards.length; row++) {
-            for (int col = 0; col < cards[row].length; col++) {
-                if (cards[row][col].points <= maxPoints && cards[row][col].handler == null) {
-                    validCards[row][col] = (cards[row][col]);
-                    count++;
-                }
-            }
-        }
-        // TODO: Ask Wesley if there is a cleaner way without count?
-        return count == 0 ? EmptyCard.getInstance()
-                : this.promptHandler.pickCard("Choose a card", validCards, player.getPlayerId());
-    }
+	public Card getEggToHatch(Card[][] cards, int maxPoints, Player player) {
+		Card[][] validCards = new Card[cards.length][cards[0].length];
+
+		boolean noValidCards = true;
+		for (int row = 0; row < cards.length; row++) {
+			for (int col = 0; col < cards[row].length; col++) {
+				if (cards[row][col].points <= maxPoints && cards[row][col].handler == null) {
+					validCards[row][col] = (cards[row][col]);
+					noValidCards = false;
+				}
+			}
+		}
+
+		if (noValidCards) {
+			return EmptyCard.getInstance();
+		} else {
+			Card choice = this.promptHandler.pickCard("Choose a card", validCards, player.getPlayerId());
+			return choice;
+		}
+	}
 }
