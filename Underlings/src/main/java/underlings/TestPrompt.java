b2b518337524b55ed2cbaefa7e1a982c1f49eb62
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-10 11:53:32 -0400

    changed TestPrompt to have optional delay time between choices and auto pick cards

--- Underlings/src/main/java/underlings/TestPrompt.java
+++ Underlings/src/main/java/underlings/TestPrompt.java
@@ -3,39 +3,70 @@ package underlings;
 import java.util.List;
 import java.util.Random;
 
+import underlings.card.Card;
 import underlings.gui.Choice;
 import underlings.gui.ConcretePrompt;
 
 public class TestPrompt extends ConcretePrompt {
-    Random rand;
+	Random rand;
+	long delay;
 
-    public TestPrompt() {
-        rand = new Random();
-    }
+	public TestPrompt() {
+		this(10);
+	}
 
-    @Override
-    public <T extends Choice> T promptChoice(String prompt, List<T> choices, int playerId) {
-        return choices.get(rand.nextInt(choices.size()));
-    }
+	public TestPrompt(long delay) {
+		rand = new Random();
+		this.delay = delay;
+	}
 
-    @Override
-    public boolean promptDecision(String question, int playerId) {
-        return rand.nextBoolean();
-    }
+	public void delay() {
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public int promptInt(String prompt, int min, int max) {
-        int val = 0;
-        do {
-            val = rand.nextInt(max);
-        } while (val < min || val > max);
-        return val;
-    }
+	@Override
+	public <T extends Choice> T promptChoice(String prompt, List<T> choices, int playerId) {
+		delay();
+		return choices.get(rand.nextInt(choices.size()));
+	}
 
-    @Override
-    public void displayMessage(String message, int playerId, int icon) {
-        if (!(message.contains("placements") || message.contains("incubation") || message.contains("applied"))) {
-            super.displayMessage(message, playerId, icon);
-        }
-    }
+	@Override
+	public boolean promptDecision(String question, int playerId) {
+		delay();
+		return rand.nextBoolean();
+	}
+
+	@Override
+	public int promptInt(String prompt, int min, int max) {
+		delay();
+		int val = 0;
+		do {
+			val = rand.nextInt(max);
+		} while (val < min || val > max);
+		return val;
+	}
+
+	@Override
+	public void displayMessage(String message, int playerId, int icon) {
+		delay();
+		if (!(message.contains("placements") || message.contains("incubation") || message.contains("applied"))) {
+			super.displayMessage(message, playerId, icon);
+		}
+	}
+
+	@Override
+	public Card pickCard(String prompt, Card[][] cards, int playerId) {
+		delay();
+		Card card = null;
+		while (card == null) {
+			int i = rand.nextInt(cards.length);
+			int j = rand.nextInt(cards[i].length);
+			card = cards[i][j];
+		}
+		return card;
+	}
 }
