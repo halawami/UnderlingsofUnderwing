7c8d03a3efb0d82918815d04b95f5d4392dc5c56
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-10 01:08:30 -0400

    pass testNoEggToHatch

--- Underlings/src/main/java/underlings/card/effect/domestic/CollectAndHatchAnyUnclaimedEggEffect.java
+++ Underlings/src/main/java/underlings/card/effect/domestic/CollectAndHatchAnyUnclaimedEggEffect.java
@@ -1,6 +1,7 @@
 package underlings.card.effect.domestic;
 
 import underlings.card.Card;
+import underlings.card.EmptyCard;
 import underlings.card.effect.HatchingGroundEffect;
 import underlings.element.ElementBag;
 import underlings.element.utilities.ElementSpaceLogic;
@@ -16,6 +17,8 @@ public class CollectAndHatchAnyUnclaimedEggEffect extends HatchingGroundEffect {
     protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag,
             ElementSpaceLogic elementSpaceLogic, Gui gui, Player currentPlayer, EggHatchingLogic eggHatchingLogic) {
         Card selectedCard = gui.getEggToHatch(hatchingGround.getAllCards(), points);
-        eggHatchingLogic.hatchEgg(selectedCard, false, currentPlayer);
+        if (selectedCard != EmptyCard.getInstance()) {
+            eggHatchingLogic.hatchEgg(selectedCard, false, currentPlayer);
+        }
     }
 }
