8606bc62d3fb025bffef70e160c1ac554b0e2f9a
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-11 15:37:23 -0400

    pass testApplyOnNoCardsInPlay

--- Underlings/src/test/java/underlings/effect/hatchingground/AddElementToAllSpacesInPlayEffectTests.java
+++ Underlings/src/test/java/underlings/effect/hatchingground/AddElementToAllSpacesInPlayEffectTests.java
@@ -19,6 +19,37 @@ import underlings.utilities.EggHatchingLogic;
 public class AddElementToAllSpacesInPlayEffectTests {
 
     @Test
+    public void testApplyOnNoCardsInPlay() {
+        ElementBag elementBag = EasyMock.mock(ElementBag.class);
+        ElementSpaceLogic elementSpaceLogic = EasyMock.mock(ElementSpaceLogic.class);
+        Card centerCard = EasyMock.mock(Card.class);
+        HatchingGround hatchingGround = EasyMock.mock(HatchingGround.class);
+        Gui gui = EasyMock.mock(Gui.class);
+        Player player = new Player(0, null, 0);
+        player.elementSpaceLogic = elementSpaceLogic;
+        List<Card> mockedCards = getMockedCards(0);
+        EggHatchingLogic eggHatchingLogic = EasyMock.mock(EggHatchingLogic.class);
+
+        AddElementToAllSpacesInPlayEffect testedEffect =
+                EasyMock.partialMockBuilder(AddElementToAllSpacesInPlayEffect.class)
+                        .addMockedMethod("addElementsToCard").createMock();
+        testedEffect.on(elementBag).on(centerCard).on(hatchingGround).on(gui).on(eggHatchingLogic).on(player);
+        testedEffect.elementColor = ElementColor.BLUE;
+
+        EasyMock.expect(hatchingGround.getAllCards()).andReturn(mockedCards);
+        for (Card mockedCard : mockedCards) {
+            testedEffect.addElementsToCard(ElementColor.BLUE, mockedCard, elementSpaceLogic, elementBag);
+        }
+        EasyMock.replay(elementBag, centerCard, elementSpaceLogic, testedEffect, gui, hatchingGround);
+        EasyMock.replay(eggHatchingLogic);
+
+        testedEffect.apply();
+
+        EasyMock.verify(elementBag, centerCard, elementSpaceLogic, testedEffect, gui, hatchingGround);
+        EasyMock.verify(eggHatchingLogic);
+    }
+
+    @Test
     public void testApplyOnOneCardInPlay() {
         ElementBag elementBag = EasyMock.mock(ElementBag.class);
         ElementSpaceLogic elementSpaceLogic = EasyMock.mock(ElementSpaceLogic.class);
