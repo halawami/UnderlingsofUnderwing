4ebbeae28bee5fb61e867cef633fbd442bb5c355
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-11 02:06:34 -0400

    add test to FinalWildPhase

---
+++ Underlings/src/test/java/underlings/phase/WildFinalPhaseTests.java
@@ -0,0 +1,29 @@
+package underlings.phase;
+
+import javax.swing.JOptionPane;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import underlings.gui.ConcretePrompt;
+import underlings.gui.Gui;
+import underlings.player.FakePlayer;
+
+public class WildFinalPhaseTests {
+
+	@Test
+	public void testRunWildFinalPhase() {
+		Gui gui = EasyMock.mock(Gui.class);
+		gui.promptHandler = EasyMock.mock(ConcretePrompt.class);
+		FinalPhase finalPhase = new WildFinalPhase(gui);
+		gui.promptHandler.displayMessage("All eggs hatched wild, the game wins", -1, JOptionPane.PLAIN_MESSAGE);
+
+		EasyMock.replay(gui, gui.promptHandler);
+
+		finalPhase.execute();
+		finalPhase.turn(FakePlayer.getInstance());
+
+		EasyMock.verify(gui, gui.promptHandler);
+	}
+
+}
