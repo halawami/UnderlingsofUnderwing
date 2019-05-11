b94b32f0b1a6b85ed0a1d2528c86590a4d5546cf
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-11 02:37:03 -0400

    pass PointTests

--- Underlings/src/test/java/underlings/scoring/PointTests.java
+++ Underlings/src/test/java/underlings/scoring/PointTests.java
@@ -3,59 +3,67 @@ package underlings.scoring;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
 import underlings.card.Card;
+import underlings.gui.Gui;
+import underlings.player.Player;
 
 public class PointTests {
 
-    private ScoreUtils scoreUtils;
+	private ScoreUtils scoreUtils;
 
-    @Before
-    public void init() {
-        this.scoreUtils = new ScoreUtils();
-    }
+	@Before
+	public void init() {
+		Player player = EasyMock.mock(Player.class);
+		List<Player> players = Arrays.asList(player);
+		Gui gui = EasyMock.mock(Gui.class);
+		List<Player> winners = Arrays.asList();
+		this.scoreUtils = new ScoreUtils(players, gui);
+	}
 
-    @Test
-    public void testEmpty() {
-        int points = this.scoreUtils.calculatePoints(Collections.emptyList());
+	@Test
+	public void testEmpty() {
+		int points = this.scoreUtils.calculatePoints(Collections.emptyList());
 
-        assertEquals(0, points);
-    }
+		assertEquals(0, points);
+	}
 
-    @Test
-    public void testOneCard() {
-        Card cardOne = new Card();
-        cardOne.points = 10;
+	@Test
+	public void testOneCard() {
+		Card cardOne = new Card();
+		cardOne.points = 10;
 
-        List<Card> cards = new ArrayList<>();
-        cards.add(cardOne);
+		List<Card> cards = new ArrayList<>();
+		cards.add(cardOne);
 
-        int points = this.scoreUtils.calculatePoints(cards);
+		int points = this.scoreUtils.calculatePoints(cards);
 
-        assertEquals(10, points);
+		assertEquals(10, points);
 
-    }
+	}
 
-    @Test
-    public void testTwoCards() {
-        Card cardOne = new Card();
-        cardOne.points = 13;
+	@Test
+	public void testTwoCards() {
+		Card cardOne = new Card();
+		cardOne.points = 13;
 
-        Card cardTwo = new Card();
-        cardTwo.points = 16;
+		Card cardTwo = new Card();
+		cardTwo.points = 16;
 
-        List<Card> cards = new ArrayList<>();
-        cards.add(cardOne);
-        cards.add(cardTwo);
+		List<Card> cards = new ArrayList<>();
+		cards.add(cardOne);
+		cards.add(cardTwo);
 
-        int points = this.scoreUtils.calculatePoints(cards);
+		int points = this.scoreUtils.calculatePoints(cards);
 
-        assertEquals(29, points);
-    }
+		assertEquals(29, points);
+	}
 
 }
