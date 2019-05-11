af66e9fb38841caf031c6d82e58f67bba29a1a4f
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-11 19:18:02 -0400

    pass testDestroyAllElementsOnClaimedCard

--- Underlings/src/main/java/underlings/card/effect/wild/DestroyAllElementsOnAllEggsInPlay.java
+++ Underlings/src/main/java/underlings/card/effect/wild/DestroyAllElementsOnAllEggsInPlay.java
@@ -9,7 +9,9 @@ public class DestroyAllElementsOnAllEggsInPlay extends AllEggsInPlayEffect {
 
     @Override
     public void applyOnCardInPlay(Card cardInPlay, ElementSpaceLogic elementSpaceLogic, ElementBag elementBag) {
-
+        for (int i = 0; i < cardInPlay.elementSpaces.length; i++) {
+            DestroyAllElementsOnSpace(cardInPlay.elementSpaces[i], elementBag);
+        }
     }
 
     public void DestroyAllElementsOnSpace(ElementSpace elementSpace, ElementBag elementBag) {
