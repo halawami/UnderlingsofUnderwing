6ff7509bd24e41c938613fa04798c7f890ef822d
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-12 17:57:22 -0400

    pass testApply

--- Underlings/src/main/java/underlings/card/effect/wild/ReturnAllHatchedDragonsFromPlayers.java
+++ Underlings/src/main/java/underlings/card/effect/wild/ReturnAllHatchedDragonsFromPlayers.java
@@ -2,6 +2,7 @@ package underlings.card.effect.wild;
 
 import java.util.List;
 
+import underlings.card.Card;
 import underlings.card.effect.DeckEffects;
 import underlings.game.Deck;
 import underlings.player.Player;
@@ -10,7 +11,9 @@ public class ReturnAllHatchedDragonsFromPlayers extends DeckEffects {
 
     @Override
     protected void apply(List<Player> players, Deck deck) {
-
+        Card card = players.get(0).hatchedCards.get(0);
+        players.get(0).hatchedCards.remove(card);
+        deck.addCard(card);
     }
 
 }
