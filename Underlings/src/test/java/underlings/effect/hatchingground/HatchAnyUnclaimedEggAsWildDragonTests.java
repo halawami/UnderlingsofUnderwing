8beef686e5599605b936c572a64465f72fa7893b
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-12 09:47:02 -0400

    fail notWantingToHatch

--- Underlings/src/test/java/underlings/effect/hatchingground/HatchAnyUnclaimedEggAsWildDragonTests.java
+++ Underlings/src/test/java/underlings/effect/hatchingground/HatchAnyUnclaimedEggAsWildDragonTests.java
@@ -39,4 +39,25 @@ public class HatchAnyUnclaimedEggAsWildDragonTests {
 
         EasyMock.verify(card, hatchingGround, elementBag, gui, player, eggHatchingLogic);
     }
+
+    @Test
+    public void notWantingToHatch() {
+        Card card = EasyMock.mock(Card.class);
+        HatchingGround hatchingGround = EasyMock.mock(HatchingGround.class);
+        ElementBag elementBag = EasyMock.mock(ElementBag.class);
+        Gui gui = EasyMock.mock(Gui.class);
+        Player player = EasyMock.mock(Player.class);
+        EggHatchingLogic eggHatchingLogic = EasyMock.mock(EggHatchingLogic.class);
+        Effect effect = new HatchAnyUnclaimedEggAsWildDragon();
+        effect.on(card).on(hatchingGround).on(elementBag).on(gui).on(player).on(eggHatchingLogic);
+        EasyMock.expect(player.getPlayerId()).andReturn(-1);
+        EasyMock.expect(gui.promptDecision("Would you like to hatch unclaimed egg as wild dragon", -1))
+                .andReturn(false);
+
+        EasyMock.replay(card, hatchingGround, elementBag, gui, player, eggHatchingLogic);
+
+        effect.apply();
+
+        EasyMock.verify(card, hatchingGround, elementBag, gui, player, eggHatchingLogic);
+    }
 }
