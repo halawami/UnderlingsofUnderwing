6813d8735f387756fd99c56fe874e351af14e64c
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-12 14:55:15 -0400

    pass ElementSpaceLogicTests.testGetPlayableElements

--- Underlings/src/test/java/underlings/element/ElementSpaceLogicTests.java
+++ Underlings/src/test/java/underlings/element/ElementSpaceLogicTests.java
@@ -1,7 +1,9 @@
 package underlings.element;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,5 +21,18 @@ public class ElementSpaceLogicTests {
 
         List<Element> actual = logic.getPlayableElements(space, playerElements);
         assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void testGetPlayableElements() {
+        List<Element> playerElements = new ArrayList<>();
+        playerElements.add(new Element(ElementColor.BLACK));
+        playerElements.add(new Element(ElementColor.WHITE));
+
+        ElementSpaceLogic logic = new ElementSpaceLogic();
+        ElementSpace space = new ElementSpace(ElementColor.BLACK);
+        List<Element> actual = logic.getPlayableElements(space, playerElements);
+        assertEquals(1, actual.size());
+        assertTrue(actual.contains(playerElements.get(0)));
     }
 }
