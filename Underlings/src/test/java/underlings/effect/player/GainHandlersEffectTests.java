e141862b7d45275bd31a0925d7b1c7981687039f
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-12 13:23:08 -0400

    fail testFourHandlers

--- Underlings/src/test/java/underlings/effect/player/GainHandlersEffectTests.java
+++ Underlings/src/test/java/underlings/effect/player/GainHandlersEffectTests.java
@@ -9,15 +9,31 @@ import underlings.card.effect.Effect;
 import underlings.card.effect.domestic.GainHandlersEffect;
 import underlings.player.Player;
 
-public class GainOneHandlerTests {
+public class GainHandlersEffectTests {
 
     @Test
-    public void testApply() {
+    public void testOneHandler() {
         Player player = EasyMock.mock(Player.class);
         GainHandlersEffect gainOneHandler = new GainHandlersEffect();
         gainOneHandler.numberOfHandlers = 1;
         gainOneHandler.on(player);
         player.gainHandler();
+        EasyMock.replay(player);
+
+        gainOneHandler.apply();
+
+        EasyMock.verify(player);
+    }
+
+    @Test
+    public void testFourHandlers() {
+        Player player = EasyMock.mock(Player.class);
+        GainHandlersEffect gainOneHandler = new GainHandlersEffect();
+        gainOneHandler.numberOfHandlers = 4;
+        gainOneHandler.on(player);
+        for (int i = 0; i < 4; i++) {
+            player.gainHandler();
+        }
         EasyMock.replay(player);
 
         gainOneHandler.apply();
