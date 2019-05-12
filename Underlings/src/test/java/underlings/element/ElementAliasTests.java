7089e7fa9325d99708fba8b8579162abea456b4b
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-12 11:38:37 -0400

    ElementAliasTests.testNoChangeRed fails

--- Underlings/src/test/java/underlings/element/ElementAliasTests.java
+++ Underlings/src/test/java/underlings/element/ElementAliasTests.java
@@ -7,10 +7,18 @@ import org.junit.Test;
 public class ElementAliasTests {
 
     @Test
-    public void testNoChange() {
+    public void testNoChangeBlue() {
         Element element = new Element(ElementColor.BLUE);
         element.setAlias(ElementColor.BLUE);
 
         assertEquals(ElementColor.BLUE, element.getAlias());
+    }
+
+    @Test
+    public void testNoChangeRed() {
+        Element element = new Element(ElementColor.RED);
+        element.setAlias(ElementColor.RED);
+
+        assertEquals(ElementColor.RED, element.getAlias());
     }
 }
