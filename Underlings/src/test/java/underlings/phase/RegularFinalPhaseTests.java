e1446be0749702000c2da033ffcfe7d91b07e987
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-11 20:17:04 -0400

    pass RegularFinalPhaseTests

--- Underlings/src/test/java/underlings/phase/RegularFinalPhaseTests.java
+++ Underlings/src/test/java/underlings/phase/RegularFinalPhaseTests.java
@@ -1,5 +1,7 @@
 package underlings.phase;
 
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 import org.easymock.EasyMock;
@@ -7,7 +9,8 @@ import org.junit.Test;
 
 import underlings.gui.ConcretePrompt;
 import underlings.gui.Gui;
-import underlings.player.FakePlayer;
+import underlings.player.Player;
+import underlings.scoring.ScoreUtils;
 
 public class RegularFinalPhaseTests {
 
@@ -15,16 +18,26 @@ public class RegularFinalPhaseTests {
     public void testRunRegularFinalPhase() {
         Gui gui = EasyMock.mock(Gui.class);
         gui.promptHandler = EasyMock.mock(ConcretePrompt.class);
+        Player player = EasyMock.mock(Player.class);
+        Phase dragonPhase = EasyMock.mock(DragonPhase.class);
+        ScoreUtils scoreUtils = EasyMock.mock(ScoreUtils.class);
 
-        FinalPhase finalPhase = new WildFinalPhase(gui);
-        gui.promptHandler.displayMessage("All eggs hatched wild, the game wins", -1, JOptionPane.PLAIN_MESSAGE);
+        FinalPhase finalPhase = new RegularFinalPhase(Arrays.asList(player), gui, dragonPhase, scoreUtils);
+        dragonPhase.setup();
+        finalPhase.turn(player);
+        gui.promptHandler.displayMessage("Game Over!", 0, JOptionPane.PLAIN_MESSAGE);
+        scoreUtils.calculateScores();
+        scoreUtils.displayScores();
+        scoreUtils.displayWinners();
+        dragonPhase.turn(player);
+        EasyMock.expectLastCall().times(2);
 
-        EasyMock.replay(gui, gui.promptHandler);
+        EasyMock.replay(gui, gui.promptHandler, player, dragonPhase, scoreUtils);
 
         finalPhase.execute();
-        finalPhase.turn(FakePlayer.getInstance());
+        finalPhase.turn(player);
 
-        EasyMock.verify(gui, gui.promptHandler);
+        EasyMock.verify(gui, gui.promptHandler, player, dragonPhase, scoreUtils);
     }
 
 }
