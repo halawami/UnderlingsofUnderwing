f8a9eb74ebcbf16de7b0c2b52fcd39c3d6157787
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-12 19:22:07 -0400

    fail testStealOneElement

--- Underlings/src/test/java/underlings/player/StealAllElementsTests.java
+++ Underlings/src/test/java/underlings/player/StealAllElementsTests.java
@@ -2,7 +2,12 @@ package underlings.player;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import underlings.effect.EffectTestUtils;
+import underlings.element.Element;
 
 public class StealAllElementsTests {
 
@@ -16,5 +21,21 @@ public class StealAllElementsTests {
         assertEquals(0, playerToStealFrom.elements.size());
         assertEquals(0, stealer.elements.size());
     }
+
+    @Test
+    public void testStealOneElement() {
+        Player playerToStealFrom = new Player(0, null, 0);
+        List<Element> elementsToBeStolen = EffectTestUtils.getMockObjects(Element.class, 1);
+        playerToStealFrom.elements = elementsToBeStolen;
+        Player stealer = new Player(0, null, 0);
+
+        stealer.stealAllElementsFromPlayer(playerToStealFrom);
+
+        assertEquals(0, playerToStealFrom.elements.size());
+        assertEquals(1, stealer.elements.size());
+        assertEquals(elementsToBeStolen.get(0), stealer.elements.get(0));
+    }
+
+
 
 }
