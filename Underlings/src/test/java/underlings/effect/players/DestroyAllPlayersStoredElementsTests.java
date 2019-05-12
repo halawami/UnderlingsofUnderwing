73734d26b75e25ca89a8db7c23f70ef421b76df5
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-12 19:58:26 -0400

    refactor DestroyAllPlayersStoredElementsTests

--- Underlings/src/test/java/underlings/effect/players/DestroyAllPlayersStoredElementsTests.java
+++ Underlings/src/test/java/underlings/effect/players/DestroyAllPlayersStoredElementsTests.java
@@ -14,21 +14,16 @@ public class DestroyAllPlayersStoredElementsTests {
 
     @Test
     public void testApplyOnTwoPlayer() {
-        List<Player> players = EffectTestUtils.getMockObjects(Player.class, 2);
-        Effect testedEffect = new DestroyAllPlayersStoredElements();
-        testedEffect.on(players);
-
-        players.forEach(Player::destroyAllElements);
-
-        players.forEach(EasyMock::replay);
-
-        testedEffect.apply();
-
-        players.forEach(EasyMock::verify);
+        testApplyOnPlayers(6);
     }
 
     @Test
     public void testApplyOnSixPlayer() {
+        testApplyOnPlayers(6);
+    }
+
+
+    public void testApplyOnPlayers(int numberOfPlayers) {
         List<Player> players = EffectTestUtils.getMockObjects(Player.class, 6);
         Effect testedEffect = new DestroyAllPlayersStoredElements();
         testedEffect.on(players);
