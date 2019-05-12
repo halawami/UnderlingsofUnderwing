21af0500b40a09aa2fe8d5a4e2d4f18973d47800
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-12 12:13:19 -0400

    testing UseBlackOrWhiteInPlaceEffect fails

--- Underlings/src/main/java/underlings/card/effect/wild/UseBlackOrWhiteInPlaceEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/UseBlackOrWhiteInPlaceEffect.java
@@ -5,11 +5,8 @@ import underlings.player.Player;
 
 public class UseBlackOrWhiteInPlaceEffect extends PlayerEffect {
 
-    // You may now use Black or White Elements in place of any other type
-
     @Override
     protected void apply(Player player) {
 
     }
-
 }
