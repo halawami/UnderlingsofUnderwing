cb3379c9fdebf0fef4004b1e4b2e565f51157f98
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-12 11:39:01 -0400

    pass testApplySameNumberOfDragons

--- Underlings/src/main/java/underlings/card/effect/wild/PlayersTradeDragon.java
+++ Underlings/src/main/java/underlings/card/effect/wild/PlayersTradeDragon.java
@@ -5,6 +5,7 @@ import java.util.List;
 import underlings.card.Card;
 import underlings.card.effect.PlayersEffect;
 import underlings.gui.Gui;
+import underlings.player.FakePlayer;
 import underlings.player.Player;
 
 public class PlayersTradeDragon extends PlayersEffect {
@@ -17,14 +18,24 @@ public class PlayersTradeDragon extends PlayersEffect {
                 playerWithMinCards = player;
             }
         }
+        int count = 0;
         for (Player player : players) {
-            if (player != playerWithMinCards) {
-                Card cardToTrade = gui.promptCard("Choose a card to trade", player.hatchedCards);
-                Card secondCardToTrade = gui.promptCard("Choose a card to trade", playerWithMinCards.hatchedCards);
-                player.hatchedCards.remove(cardToTrade);
-                playerWithMinCards.hatchedCards.add(cardToTrade);
-                player.hatchedCards.add(secondCardToTrade);
-                playerWithMinCards.hatchedCards.remove(secondCardToTrade);
+            if (player != playerWithMinCards && playerWithMinCards.hatchedCards.size() == player.hatchedCards.size()) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            gui.notifyAction(FakePlayer.getInstance().getPlayerId(), "There is no player with the least dragons");
+        } else {
+            for (Player player : players) {
+                if (player != playerWithMinCards) {
+                    Card cardToTrade = gui.promptCard("Choose a card to trade", player.hatchedCards);
+                    Card secondCardToTrade = gui.promptCard("Choose a card to trade", playerWithMinCards.hatchedCards);
+                    player.hatchedCards.remove(cardToTrade);
+                    playerWithMinCards.hatchedCards.add(cardToTrade);
+                    player.hatchedCards.add(secondCardToTrade);
+                    playerWithMinCards.hatchedCards.remove(secondCardToTrade);
+                }
             }
         }
     }
