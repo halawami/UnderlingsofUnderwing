cf6c86f7060d6986db972d1c28a3c3886a25e0bb
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-13 01:17:36 -0400

    refactor ReturnMostValuableDragonEffectTests

--- Underlings/src/test/java/underlings/effect/deck/ReturnMostValuableDragonEffectTests.java
+++ Underlings/src/test/java/underlings/effect/deck/ReturnMostValuableDragonEffectTests.java
@@ -15,27 +15,16 @@ public class ReturnMostValuableDragonEffectTests {
 
     @Test
     public void testApplyEffectOnTwoPlayers() {
-        List<Player> players = TestUtils.mockListOf(Player.class).withLength(2);
-        Deck deck = EasyMock.mock(Deck.class);
-        Gui gui = EasyMock.mock(Gui.class);
-        ReturnMostValuableDragonEffect testedEffect = EasyMock.partialMockBuilder(ReturnMostValuableDragonEffect.class)
-                .addMockedMethod("returnMostValueDragon").createMock();
-        testedEffect.on(players).on(deck).on(gui);
-
-        players.forEach(player -> testedEffect.returnMostValueDragon(player, deck, gui));
-
-        EasyMock.replay(deck, gui, testedEffect);
-        players.forEach(EasyMock::replay);
-
-        testedEffect.apply();
-
-        EasyMock.verify(deck, gui, testedEffect);
-        players.forEach(EasyMock::verify);
+        this.testApplyEffectOnPlayers(2);
     }
 
     @Test
     public void testApplyEffectOnSixPlayers() {
-        List<Player> players = TestUtils.mockListOf(Player.class).withLength(6);
+        this.testApplyEffectOnPlayers(6);
+    }
+
+    public void testApplyEffectOnPlayers(int numberOfPlayers) {
+        List<Player> players = TestUtils.mockListOf(Player.class).withLength(numberOfPlayers);
         Deck deck = EasyMock.mock(Deck.class);
         Gui gui = EasyMock.mock(Gui.class);
         ReturnMostValuableDragonEffect testedEffect = EasyMock.partialMockBuilder(ReturnMostValuableDragonEffect.class)
