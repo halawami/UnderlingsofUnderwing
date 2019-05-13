a20aa79f0a8e1d1a12199fad13d20a7e1aa7a42f
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-13 14:22:13 -0400

    pass testMostValuableMultipleDragonsTie

--- Underlings/src/main/java/underlings/player/Player.java
+++ Underlings/src/main/java/underlings/player/Player.java
@@ -143,7 +143,9 @@ public class Player {
         Map<Integer, List<Card>> highestValues = new HashMap<Integer, List<Card>>();
         int max = -1;
         for (Card card : this.hatchedCards) {
-            highestValues.put(card.points, new LinkedList<>());
+            if (!highestValues.containsKey(card.points)) {
+                highestValues.put(card.points, new LinkedList<>());
+            }
             if (card.points >= max) {
                 highestValues.get(card.points).add(card);
                 max = card.points;
