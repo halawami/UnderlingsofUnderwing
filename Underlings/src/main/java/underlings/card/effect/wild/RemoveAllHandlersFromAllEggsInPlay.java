6be34928c89d1ba4efd9673a9b9a43b6ffcf623b
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-13 04:16:12 -0400

    pass testWildHandler

--- Underlings/src/main/java/underlings/card/effect/wild/RemoveAllHandlersFromAllEggsInPlay.java
+++ Underlings/src/main/java/underlings/card/effect/wild/RemoveAllHandlersFromAllEggsInPlay.java
@@ -5,12 +5,13 @@ import underlings.card.effect.wild.alleggsinplay.AllEggsInPlayEffect;
 import underlings.element.ElementBag;
 import underlings.element.utilities.ElementSpaceLogic;
 import underlings.handler.HandlerState;
+import underlings.handler.WildHandler;
 
 public class RemoveAllHandlersFromAllEggsInPlay extends AllEggsInPlayEffect {
 
     @Override
     public void applyOnCardInPlay(Card cardInPlay, ElementSpaceLogic elementSpaceLogic, ElementBag elementBag) {
-        if (cardInPlay.handler != null) {
+        if (cardInPlay.handler != null && cardInPlay.handler != WildHandler.getInstance()) {
             cardInPlay.handler.moveToState(HandlerState.BREAK_ROOM);
             cardInPlay.handler = null;
         }
