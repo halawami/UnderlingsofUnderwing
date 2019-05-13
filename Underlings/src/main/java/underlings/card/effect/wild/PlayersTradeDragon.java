343d0b7473e450879097545c19d5e0310470ca01
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-13 17:27:43 -0400

    kill mutant in PlayersTradeDragon

--- Underlings/src/main/java/underlings/card/effect/wild/PlayersTradeDragon.java
+++ Underlings/src/main/java/underlings/card/effect/wild/PlayersTradeDragon.java
@@ -1,6 +1,9 @@
 package underlings.card.effect.wild;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import underlings.card.Card;
 import underlings.card.effect.PlayersEffect;
@@ -11,23 +14,25 @@ import underlings.utilities.LocaleWrap;
 
 public class PlayersTradeDragon extends PlayersEffect {
 
+    // TODO: clean up
     @Override
     protected void apply(Player currentPlayer, List<Player> players, Gui gui) {
         Player playerWithMinCards = players.get(0);
+        int min = playerWithMinCards.hatchedCards.size();
+        Map<Integer, List<Player>> highestCards = new HashMap<Integer, List<Player>>();
         for (Player player : players) {
-            if (player.hatchedCards.size() < playerWithMinCards.hatchedCards.size()) {
-                playerWithMinCards = player;
+            if (!highestCards.containsKey(player.hatchedCards.size())) {
+                highestCards.put(player.hatchedCards.size(), new LinkedList<>());
+            }
+            if (player.hatchedCards.size() <= min) {
+                highestCards.get(player.hatchedCards.size()).add(player);
+                min = player.hatchedCards.size();
             }
         }
-        int count = 0;
-        for (Player player : players) {
-            if (player != playerWithMinCards && playerWithMinCards.hatchedCards.size() == player.hatchedCards.size()) {
-                count++;
-            }
-        }
-        if (count > 0) {
+        if (highestCards.get(min).size() > 1) {
             gui.notifyAction(FakePlayer.getInstance().getPlayerId(), LocaleWrap.get("notify_no_player_least_dragons"));
         } else {
+            playerWithMinCards = highestCards.get(min).get(0);
             for (Player player : players) {
                 if (player != playerWithMinCards) {
                     Card cardToTrade = gui.promptCard(LocaleWrap.get("prompt_card_to_trade"), player.hatchedCards);
