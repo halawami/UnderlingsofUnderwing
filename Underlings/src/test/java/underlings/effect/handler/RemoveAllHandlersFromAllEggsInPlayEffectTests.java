50ba30a40bb82eaf002856a128812932608e985b
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-13 04:10:59 -0400

    fail testPlayerHandler

--- Underlings/src/test/java/underlings/effect/handler/RemoveAllHandlersFromAllEggsInPlayEffectTests.java
+++ Underlings/src/test/java/underlings/effect/handler/RemoveAllHandlersFromAllEggsInPlayEffectTests.java
@@ -1,9 +1,12 @@
 package underlings.effect.handler;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import underlings.card.Card;
 import underlings.card.effect.wild.RemoveAllHandlersFromAllEggsInPlay;
+import underlings.handler.Handler;
+import underlings.handler.HandlerState;
 
 public class RemoveAllHandlersFromAllEggsInPlayEffectTests {
 
@@ -14,6 +17,19 @@ public class RemoveAllHandlersFromAllEggsInPlayEffectTests {
         RemoveAllHandlersFromAllEggsInPlay testedEffect = new RemoveAllHandlersFromAllEggsInPlay();
 
         testedEffect.applyOnCardInPlay(cardWithNoHandler, null, null);
+    }
+
+    @Test
+    public void testPlayerHandler() {
+        Card card = new Card();
+        Handler handler = new Handler(HandlerState.CARD);
+        card.handler = handler;
+        RemoveAllHandlersFromAllEggsInPlay testedEffect = new RemoveAllHandlersFromAllEggsInPlay();
+
+        testedEffect.applyOnCardInPlay(card, null, null);
+
+        Assert.assertNull(card.handler);
+        Assert.assertEquals(HandlerState.BREAK_ROOM, handler);
     }
 
 }
