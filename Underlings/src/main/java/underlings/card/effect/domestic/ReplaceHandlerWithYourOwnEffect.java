fb8cde607ff711f2573ac82a71d3332718e0ed4b
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-13 23:03:53 -0400

    pass testApplyEffectNoSelectableHandler

--- Underlings/src/main/java/underlings/card/effect/domestic/ReplaceHandlerWithYourOwnEffect.java
+++ Underlings/src/main/java/underlings/card/effect/domestic/ReplaceHandlerWithYourOwnEffect.java
@@ -1,5 +1,7 @@
 package underlings.card.effect.domestic;
 
+import java.util.List;
+
 import underlings.card.Card;
 import underlings.card.effect.HandlerEffect;
 import underlings.game.HatchingGround;
@@ -14,9 +16,14 @@ public class ReplaceHandlerWithYourOwnEffect extends HandlerEffect {
     @Override
     protected void apply(Player currentPlayer, HatchingGround hatchingGround, HandlerMovementLogic handlerLogic,
             Gui gui) {
-        Card chosenEgg = gui.promptCard("Choose a card to replace its handler", hatchingGround.getClaimedEggs());
-        Handler chosenHandler = gui.promptHandler("Choose a handler to replace with", 0, currentPlayer.getHandlers());
-        handlerLogic.move(chosenEgg.handler, HandlerChoice.BREAK_ROOM, currentPlayer.getPlayerId());
-        handlerLogic.moveToCard(chosenHandler, chosenEgg);
+        List<Card> claimedEggs = hatchingGround.getClaimedEggs();
+        if (!claimedEggs.isEmpty()) {
+            Card chosenEgg = gui.promptCard("Choose a card to replace its handler", claimedEggs);
+            List<Handler> handlerChoices = currentPlayer.getHandlers();
+            Handler chosenHandler = gui.promptHandler("Choose a handler to replace with",
+                    currentPlayer.getPlayerId(), handlerChoices);
+            handlerLogic.move(chosenEgg.handler, HandlerChoice.BREAK_ROOM, currentPlayer.getPlayerId());
+            handlerLogic.moveToCard(chosenHandler, chosenEgg);
+        }
     }
 }
