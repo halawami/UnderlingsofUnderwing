3918c5957b4a182332766409970fd666e01081c6
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-13 23:40:09 -0400

    fail testEffect

---
+++ Underlings/src/main/java/underlings/card/effect/domestic/CollectFiveElementsEffect.java
@@ -0,0 +1,16 @@
+package underlings.card.effect.domestic;
+
+import underlings.card.effect.ElementEffect;
+import underlings.element.ElementBag;
+import underlings.element.ElementColor;
+import underlings.player.Player;
+
+public class CollectFiveElementsEffect extends ElementEffect {
+
+    public ElementColor[] elementColors;
+
+    @Override
+    protected void apply(Player player, ElementBag elementBag) {
+
+    }
+}
---
+++ Underlings/src/test/java/underlings/effect/element/CollectFiveElementsEffectTests.java
@@ -0,0 +1,46 @@
+package underlings.effect.element;
+
+import static underlings.element.ElementColor.BLACK;
+import static underlings.element.ElementColor.BLUE;
+import static underlings.element.ElementColor.GREEN;
+import static underlings.element.ElementColor.RED;
+import static underlings.element.ElementColor.WHITE;
+
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import underlings.TestUtils;
+import underlings.card.effect.domestic.CollectFiveElementsEffect;
+import underlings.element.Element;
+import underlings.element.ElementBag;
+import underlings.element.ElementColor;
+import underlings.player.Player;
+
+public class CollectFiveElementsEffectTests {
+
+    @Test
+    public void testEffect() {
+        ElementBag elementBag = EasyMock.mock(ElementBag.class);
+        Player player = EasyMock.mock(Player.class);
+        CollectFiveElementsEffect testedEffect = new CollectFiveElementsEffect();
+        ElementColor[] colors = new ElementColor[]{RED, BLUE, GREEN, BLACK, WHITE};
+        testedEffect.elementColors = colors;
+        List<Element> mockElements = TestUtils.mockListOf(Element.class).withLength(5);
+
+        for (int i = 0; i < 5; i++) {
+            EasyMock.expect(elementBag.drawElementFromList(colors[i])).andReturn(mockElements.get(i));
+            player.addElement(mockElements.get(i));
+        }
+
+        EasyMock.replay(elementBag, player);
+        mockElements.forEach(EasyMock::replay);
+
+        testedEffect.on(elementBag).on(player).apply();
+
+        EasyMock.verify(elementBag, player);
+        mockElements.forEach(EasyMock::verify);
+    }
+
+}
