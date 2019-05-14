6e133c5daac81d9e3e4da5c595f96e347ac585c3
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-14 11:25:04 -0400

    add extra card to json and project

---
+++ Underlings/src/main/java/underlings/card/effect/domestic/NextEggHatchesImmediatelyEffect.java
@@ -0,0 +1,13 @@
+package underlings.card.effect.domestic;
+
+import underlings.card.effect.PlayerEffect;
+import underlings.player.Player;
+
+public class NextEggHatchesImmediatelyEffect extends PlayerEffect {
+
+    @Override
+    protected void apply(Player player) {
+
+    }
+
+}
