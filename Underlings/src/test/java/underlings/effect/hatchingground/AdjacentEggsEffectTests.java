c56f5f0d52f15a77f3018b63dfb4b94dc591c36f
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-14 12:48:44 -0400

    fix testApplyOnMultipleAdjacentCards

--- Underlings/src/test/java/underlings/effect/hatchingground/AdjacentEggsEffectTests.java
+++ Underlings/src/test/java/underlings/effect/hatchingground/AdjacentEggsEffectTests.java
@@ -35,7 +35,7 @@ public class AdjacentEggsEffectTests {
         EasyMock.expect(hatchingGround.getAdjacentCards(centerCard)).andReturn(mockedCards);
         for (Card mockedCard : mockedCards) {
             testedEffect.applyOnAdjacentEgg(mockedCard, elementBag, player.elementSpaceLogic,
-                    eggHatchingLogic,  null, null, null);
+                    eggHatchingLogic, null, null, hatchingGround);
         }
 
         EasyMock.replay(centerCard, hatchingGround, elementBag, player.elementSpaceLogic, testedEffect, gui);
