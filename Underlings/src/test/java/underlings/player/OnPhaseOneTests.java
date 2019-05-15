7c34c76994fda43b7bc7f433084a72b83a4e6e4b
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-14 23:41:28 -0400

    refactor OnPhaseOneTests

--- Underlings/src/test/java/underlings/player/OnPhaseOneTests.java
+++ Underlings/src/test/java/underlings/player/OnPhaseOneTests.java
@@ -12,29 +12,21 @@ public class OnPhaseOneTests {
 
     @Test
     public void testNoObservers() {
-        //TODO: find a better way of testing this
-        Player player = new Player(0, null, 0);
-        player.onPhaseOne();
+        this.testWithObservers(0);
     }
 
     @Test
     public void testOneObserver() {
-        ObserverEffect observerEffect = EasyMock.mock(ObserverEffect.class);
-        Player player = new Player(0, null, 0);
-        player.addObserverEffect(observerEffect);
-
-        observerEffect.onPhaseOne(player);
-
-        EasyMock.replay(observerEffect);
-
-        player.onPhaseOne();
-
-        EasyMock.verify(observerEffect);
+        this.testWithObservers(1);
     }
 
     @Test
     public void testTwoObserver() {
-        List<ObserverEffect> observerEffects = TestUtils.mockListOf(ObserverEffect.class).withLength(2);
+        this.testWithObservers(2);
+    }
+
+    private void testWithObservers(int numberOfObservers) {
+        List<ObserverEffect> observerEffects = TestUtils.mockListOf(ObserverEffect.class).withLength(numberOfObservers);
         Player player = new Player(0, null, 0);
         observerEffects.forEach(player::addObserverEffect);
 
