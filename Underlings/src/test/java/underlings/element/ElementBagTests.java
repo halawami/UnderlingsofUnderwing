0cdd9b5ade7042a42cd5c13d92c8df06432af703
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-15 01:48:03 -0400

    fail testAllButOneAvailableDrawChoices

--- Underlings/src/test/java/underlings/element/ElementBagTests.java
+++ Underlings/src/test/java/underlings/element/ElementBagTests.java
@@ -2,6 +2,12 @@ package underlings.element;
 
 import static org.junit.Assert.assertEquals;
 import static underlings.gui.DrawChoice.BLUE;
+import static underlings.gui.DrawChoice.GREEN;
+import static underlings.gui.DrawChoice.ORANGE;
+import static underlings.gui.DrawChoice.PURPLE;
+import static underlings.gui.DrawChoice.RED;
+import static underlings.gui.DrawChoice.WHITE;
+import static underlings.gui.DrawChoice.YELLOW;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -78,5 +84,27 @@ public class ElementBagTests {
 
         List<DrawChoice> availableDrawChoices = bag.getAvailableDrawChoices();
         assertEquals(Arrays.asList(BLUE), availableDrawChoices);
+    }
+
+    @Test
+    public void testAllButOneAvailableDrawChoices() throws NoSuchFieldException, IllegalAccessException {
+        ElementBag bag = new ElementBag(new ElementFactory(), new Random());
+        Field privateMap = ElementBag.class.getDeclaredField("elementCount");
+        privateMap.setAccessible(true);
+
+        @SuppressWarnings("unchecked")
+        Map<ElementColor, Integer> elementCounts = (Map<ElementColor, Integer>) privateMap.get(bag);
+        elementCounts.put(ElementColor.BLUE, 1);
+        elementCounts.put(ElementColor.RED, 1);
+        elementCounts.put(ElementColor.YELLOW, 1);
+        elementCounts.put(ElementColor.PURPLE, 1);
+        elementCounts.put(ElementColor.GREEN, 1);
+        elementCounts.put(ElementColor.ORANGE, 1);
+        elementCounts.put(ElementColor.WHITE, 1);
+        elementCounts.put(ElementColor.BLACK, 0);
+
+        List<DrawChoice> availableDrawChoices = bag.getAvailableDrawChoices();
+        List<DrawChoice> expectedDrawChoices = Arrays.asList(BLUE, RED, YELLOW, PURPLE, GREEN, ORANGE, WHITE);
+        assertEquals(expectedDrawChoices, availableDrawChoices);
     }
 }
