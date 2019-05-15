351198626c4ef8464c2e6feb1a8811e51d796ed5
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-15 01:41:03 -0400

    fail testNoAvailableDrawChoices

--- Underlings/src/test/java/underlings/element/ElementBagTests.java
+++ Underlings/src/test/java/underlings/element/ElementBagTests.java
@@ -3,10 +3,13 @@ package underlings.element;
 import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import org.junit.Test;
+
+import underlings.gui.DrawChoice;
 
 public class ElementBagTests {
 
@@ -18,7 +21,7 @@ public class ElementBagTests {
         privateMap.setAccessible(true);
 
         @SuppressWarnings("unchecked")
-        Map<ElementColor, Integer> elementCounts = (Map<ElementColor, Integer>) privateMap.get(bag);;
+        Map<ElementColor, Integer> elementCounts = (Map<ElementColor, Integer>) privateMap.get(bag);
         elementCounts.put(ElementColor.BLUE, 0);
         elementCounts.put(ElementColor.RED, 0);
         elementCounts.put(ElementColor.YELLOW, 0);
@@ -31,5 +34,26 @@ public class ElementBagTests {
         Element actual = bag.drawRandomElement();
         Element expected = NullElement.getInstance();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNoAvailableDrawChoices() throws NoSuchFieldException, IllegalAccessException {
+        ElementBag bag = new ElementBag(new ElementFactory(), new Random());
+        Field privateMap = ElementBag.class.getDeclaredField("elementCount");
+        privateMap.setAccessible(true);
+
+        @SuppressWarnings("unchecked")
+        Map<ElementColor, Integer> elementCounts = (Map<ElementColor, Integer>) privateMap.get(bag);
+        elementCounts.put(ElementColor.BLUE, 0);
+        elementCounts.put(ElementColor.RED, 0);
+        elementCounts.put(ElementColor.YELLOW, 0);
+        elementCounts.put(ElementColor.PURPLE, 0);
+        elementCounts.put(ElementColor.GREEN, 0);
+        elementCounts.put(ElementColor.ORANGE, 0);
+        elementCounts.put(ElementColor.WHITE, 0);
+        elementCounts.put(ElementColor.BLACK, 0);
+
+        List<DrawChoice> availableDrawChoices = bag.getAvailableDrawChoices();
+        assertEquals(0, availableDrawChoices.size());
     }
 }
