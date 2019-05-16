df86620f0a9115f99e7af6de1093169f9443bdf8
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-16 19:49:21 -0400

    HatchingGroundTests.testReplaceCardBigger fails

--- Underlings/src/test/java/underlings/hatchingground/HatchingGroundTests.java
+++ Underlings/src/test/java/underlings/hatchingground/HatchingGroundTests.java
@@ -28,4 +28,23 @@ public class HatchingGroundTests {
         assertEquals(card2, hatchingGround.cards[0][0]);
         EasyMock.verify(deck);
     }
+
+    @Test
+    public void testReplaceCardBigger() {
+        Deck deck = EasyMock.mock(Deck.class);
+        Card card1 = new Card();
+        Card card2 = new Card();
+
+        EasyMock.expect(deck.draw()).andReturn(new Card()).times(3);
+        EasyMock.expect(deck.draw()).andReturn(card1);
+        EasyMock.expect(deck.draw()).andReturn(card2);
+
+        EasyMock.replay(deck);
+        HatchingGround hatchingGround = new HatchingGround(deck, null);
+        hatchingGround.setDimensions(2, 2);
+        hatchingGround.populate();
+        hatchingGround.replaceCard(card1);
+        assertEquals(card2, hatchingGround.cards[1][1]);
+        EasyMock.verify(deck);
+    }
 }
