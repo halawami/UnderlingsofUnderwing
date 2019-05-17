a59789958208b29dff811be89bc6da373e680af6
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-16 20:21:42 -0400

    GetElementSpaceWithColorsTests.testOneSpaceOneColorInvalid passes

--- Underlings/src/test/java/underlings/gui/GetElementSpaceWithColorsTests.java
+++ Underlings/src/test/java/underlings/gui/GetElementSpaceWithColorsTests.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.junit.Test;
 
 import underlings.card.Card;
+import underlings.element.Element;
 import underlings.element.ElementColor;
 import underlings.element.ElementSpace;
 
@@ -33,6 +34,21 @@ public class GetElementSpaceWithColorsTests {
         card.elementSpaces[0] = new ElementSpace(ElementColor.RED);
 
         ElementColor[] colorChoices = {};
+
+        Gui gui = new Gui(null, null);
+        List<Card> cards = Arrays.asList(card);
+        ElementSpace space = gui.getElementSpaceWithColors(cards, colorChoices);
+        assertEquals(null, space);
+    }
+
+    @Test
+    public void testOneSpaceOneColorInvalid() {
+        Card card = new Card();
+        card.elementSpaces = new ElementSpace[1];
+        card.elementSpaces[0] = new ElementSpace(ElementColor.RED);
+        card.elementSpaces[0].addElements(new Element(ElementColor.RED));
+
+        ElementColor[] colorChoices = {ElementColor.BLUE};
 
         Gui gui = new Gui(null, null);
         List<Card> cards = Arrays.asList(card);
