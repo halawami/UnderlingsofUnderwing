8704d86d6163101312b7c10ab38686a658f75e01
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-16 21:59:01 -0400

    TwoHandlersOnFieldSpaceEffectTests.testEffect fails

---
+++ Underlings/src/main/java/underlings/card/effect/domestic/TwoHandlersOnFieldSpaceEffect.java
@@ -0,0 +1,13 @@
+package underlings.card.effect.domestic;
+
+import underlings.card.effect.PlayerEffect;
+import underlings.player.Player;
+
+public class TwoHandlersOnFieldSpaceEffect extends PlayerEffect {
+
+    @Override
+    protected void apply(Player player) {
+
+    }
+
+}
