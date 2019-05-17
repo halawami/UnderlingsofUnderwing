df0920231448e1f6ef73c56fe1f4cfcf0b12a018
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2019-05-16 21:21:42 -0400

    GetElementSpaceWithColorsTests.testCardWithValidAndInvalidSpaces fails

--- Underlings/src/test/java/underlings/gui/GetElementSpaceWithColorsTests.java
+++ Underlings/src/test/java/underlings/gui/GetElementSpaceWithColorsTests.java
@@ -170,4 +170,33 @@ public class GetElementSpaceWithColorsTests {
         EasyMock.verify(prompt);
     }
 
+    @Test
+    public void testCardWithValidAndInvalidSpaces() {
+        Card card = new Card();
+        card.name = "TestCard";
+        card.elementSpaces = new ElementSpace[2];
+        card.elementSpaces[0] = new ElementSpace(ElementColor.ORANGE);
+        card.elementSpaces[0].addElements(new Element(ElementColor.RED));
+        card.elementSpaces[1] = new ElementSpace(ElementColor.BLUE);
+        card.elementSpaces[1].addElements(new Element(ElementColor.BLUE));
+
+        ElementColor[] colorChoices = {ElementColor.RED};
+
+        PromptHandler prompt = EasyMock.mock(PromptHandler.class);
+        EasyMock.expect(
+                prompt.promptChoice("Would you like to take an element from a card?", YesNoChoice.getChoices(), 2))
+                .andReturn(YesNoChoice.YES);
+        EasyMock.expect(prompt.promptChoice("Pick a card to take from", Arrays.asList(card), 2)).andReturn(card);
+        EasyMock.expect(prompt.promptChoice("Pick a space to take from", Arrays.asList(card.elementSpaces[0]), 2))
+                .andReturn(card.elementSpaces[0]);
+        EasyMock.replay(prompt);
+
+        Gui gui = new Gui(prompt, null);
+        List<Card> cards = Arrays.asList(card);
+        ElementSpace space = gui.getElementSpaceWithColors(cards, colorChoices, 2);
+        assertEquals(card.elementSpaces[0], space);
+
+        EasyMock.verify(prompt);
+    }
+
 }
