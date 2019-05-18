b89216b33fee160bf1b7ad01264f518437ecf83c
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-18 09:42:45 -0400

    fail testNoHandler

---
+++ Underlings/src/main/java/underlings/card/effect/wild/ReturnAllAdjacentHandlersEffect.java
@@ -0,0 +1,20 @@
+package underlings.card.effect.wild;
+
+import underlings.card.Card;
+import underlings.card.effect.wild.adjacenteggs.AdjacentEggsEffect;
+import underlings.element.ElementBag;
+import underlings.element.utilities.ElementSpaceLogic;
+import underlings.game.Deck;
+import underlings.game.HatchingGround;
+import underlings.handler.HandlerMovementLogic;
+import underlings.utilities.EggHatchingLogic;
+
+public class ReturnAllAdjacentHandlersEffect extends AdjacentEggsEffect {
+
+    @Override
+    public void applyOnAdjacentEgg(Card adjacentEgg, ElementBag elementBag, ElementSpaceLogic elementSpaceLogic,
+            EggHatchingLogic eggHatchingLogic, Deck deck, HandlerMovementLogic handlerMovementLogic,
+            HatchingGround hatchingGround) {
+
+    }
+}
---
+++ Underlings/src/test/java/underlings/effect/hatchingground/ReturnAllAdjacentHandlersEffectTests.java
@@ -0,0 +1,30 @@
+package underlings.effect.hatchingground;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import underlings.card.Card;
+import underlings.card.effect.wild.ReturnAllAdjacentHandlersEffect;
+import underlings.handler.HandlerChoice;
+import underlings.handler.HandlerMovementLogic;
+import underlings.player.FakePlayer;
+
+public class ReturnAllAdjacentHandlersEffectTests {
+
+    @Test
+    public void testNoHandler() {
+        Card adjacentCard = EasyMock.mock(Card.class);
+        HandlerMovementLogic handlerMovementLogic = EasyMock.mock(HandlerMovementLogic.class);
+
+        handlerMovementLogic.move(adjacentCard.handler, HandlerChoice.READY_ROOM, FakePlayer.getInstance());
+        handlerMovementLogic.removeHandlerFromCard(adjacentCard);
+
+        EasyMock.replay(adjacentCard, handlerMovementLogic);
+
+        ReturnAllAdjacentHandlersEffect testedEffect = new ReturnAllAdjacentHandlersEffect();
+        testedEffect.applyOnAdjacentEgg(adjacentCard, null, null, null, null, handlerMovementLogic, null);
+
+        EasyMock.verify(adjacentCard, handlerMovementLogic);
+    }
+
+}
