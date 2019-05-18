b2ae1400edcca1dbff7e2679728a9cba1223d53b
George Main IV <mainge@rose-hulman.edu>
2019-05-18 12:43:23 -0400

    refactor TwoHandlersOnFieldSpaceEffectTest

--- Underlings/src/test/java/underlings/card/effect/domestic/TwoHandlersOnFieldSpaceEffectTests.java
+++ Underlings/src/test/java/underlings/card/effect/domestic/TwoHandlersOnFieldSpaceEffectTests.java
@@ -1,18 +1,16 @@
 package underlings.card.effect.domestic;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
-
+import underlings.Constructors;
 import underlings.card.effect.Effect;
-import underlings.handler.HandlerFactory;
 import underlings.player.Player;
 
 public class TwoHandlersOnFieldSpaceEffectTests {
 
     @Test
     public void testEffect() {
-        Player player = new Player(6, new HandlerFactory(), 2);
+        Player player = Constructors.Player();
 
         Effect effect = new TwoHandlersOnFieldSpaceEffect();
         effect.on(player).apply();
