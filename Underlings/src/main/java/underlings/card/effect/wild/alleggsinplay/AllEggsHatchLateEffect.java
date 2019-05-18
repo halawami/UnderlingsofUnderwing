75334670052c2bd1e85d9454fe8710d061631512
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-05-18 07:49:11 -0400

    remove gui.alert from effect

--- Underlings/src/main/java/underlings/card/effect/wild/alleggsinplay/AllEggsHatchLateEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/alleggsinplay/AllEggsHatchLateEffect.java
@@ -3,15 +3,12 @@ package underlings.card.effect.wild.alleggsinplay;
 import underlings.card.effect.HatchingGroundEffect;
 import underlings.game.HatchingGround;
 import underlings.gui.Gui;
-import underlings.gui.Gui.PromptType;
-import underlings.utilities.LocaleWrap;
 
 public class AllEggsHatchLateEffect extends HatchingGroundEffect {
 
     @Override
     protected void apply(HatchingGround hatchingGround, Gui gui) {
         hatchingGround.lateHatching = true;
-        gui.alert(LocaleWrap.get("eggs_hatch_late"), PromptType.WARNING);
     }
 
 }
--- Underlings/src/test/java/underlings/effect/hatchingground/AllEggsHatchLateEffectTests.java
+++ Underlings/src/test/java/underlings/effect/hatchingground/AllEggsHatchLateEffectTests.java
@@ -8,8 +8,6 @@ import org.junit.Test;
 import underlings.card.effect.wild.alleggsinplay.AllEggsHatchLateEffect;
 import underlings.game.HatchingGround;
 import underlings.gui.Gui;
-import underlings.gui.Gui.PromptType;
-import underlings.utilities.LocaleWrap;
 
 public class AllEggsHatchLateEffectTests {
 
@@ -19,7 +17,6 @@ public class AllEggsHatchLateEffectTests {
         Gui gui = EasyMock.mock(Gui.class);
         AllEggsHatchLateEffect effect = new AllEggsHatchLateEffect();
         effect.on(hatchingGround).on(gui);
-        gui.alert(LocaleWrap.get("eggs_hatch_late"), PromptType.WARNING);
         EasyMock.replay(hatchingGround, gui);
 
         effect.apply();
