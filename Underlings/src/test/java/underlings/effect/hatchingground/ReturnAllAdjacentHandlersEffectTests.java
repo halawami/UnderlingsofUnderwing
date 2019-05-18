97b8f15dcbdd1b10318bb843ae27af228fbfb862
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-18 09:45:05 -0400

    pass testWildHandler

--- Underlings/src/test/java/underlings/effect/hatchingground/ReturnAllAdjacentHandlersEffectTests.java
+++ Underlings/src/test/java/underlings/effect/hatchingground/ReturnAllAdjacentHandlersEffectTests.java
@@ -7,6 +7,7 @@ import underlings.card.Card;
 import underlings.card.effect.wild.ReturnAllAdjacentHandlersEffect;
 import underlings.handler.HandlerChoice;
 import underlings.handler.HandlerMovementLogic;
+import underlings.handler.WildHandler;
 import underlings.player.FakePlayer;
 
 public class ReturnAllAdjacentHandlersEffectTests {
@@ -14,6 +15,23 @@ public class ReturnAllAdjacentHandlersEffectTests {
     @Test
     public void testNoHandler() {
         Card adjacentCard = EasyMock.mock(Card.class);
+        HandlerMovementLogic handlerMovementLogic = EasyMock.mock(HandlerMovementLogic.class);
+
+        handlerMovementLogic.move(adjacentCard.handler, HandlerChoice.READY_ROOM, FakePlayer.getInstance());
+        handlerMovementLogic.removeHandlerFromCard(adjacentCard);
+
+        EasyMock.replay(adjacentCard, handlerMovementLogic);
+
+        ReturnAllAdjacentHandlersEffect testedEffect = new ReturnAllAdjacentHandlersEffect();
+        testedEffect.applyOnAdjacentEgg(adjacentCard, null, null, null, null, handlerMovementLogic, null);
+
+        EasyMock.verify(adjacentCard, handlerMovementLogic);
+    }
+
+    @Test
+    public void testWildHandler() {
+        Card adjacentCard = EasyMock.mock(Card.class);
+        adjacentCard.handler = WildHandler.getInstance();
         HandlerMovementLogic handlerMovementLogic = EasyMock.mock(HandlerMovementLogic.class);
 
         handlerMovementLogic.move(adjacentCard.handler, HandlerChoice.READY_ROOM, FakePlayer.getInstance());
