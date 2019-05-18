74689c510e39319bd2901f27905ac4dec428099e
George Main IV <mainge@rose-hulman.edu>
2019-05-18 12:04:55 -0400

    create mock test framework

---
+++ Underlings/src/test/java/underlings/MockTest.java
@@ -0,0 +1,27 @@
+package underlings;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.easymock.EasyMock;
+import org.junit.After;
+
+public abstract class MockTest {
+
+    protected List<Object> mocks = new ArrayList<>();
+
+    public <T> T getMock(Class<T> mockClass) {
+        T mock = EasyMock.mock(mockClass);
+        this.mocks.add(mock);
+        return mock;
+    }
+
+    public void replay() {
+        EasyMock.replay(this.mocks.toArray());
+    }
+
+    @After
+    public void verify() {
+        EasyMock.verify(this.mocks.toArray());
+    }
+
+}
