e01c1571506f2a80e70ffa94aa939852422b8127
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-18 11:09:49 -0400

    pass testTwoCards

--- Underlings/src/test/java/underlings/effect/deck/ReorderTopThreeCardsEffectTests.java
+++ Underlings/src/test/java/underlings/effect/deck/ReorderTopThreeCardsEffectTests.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import underlings.TestUtils;
 import underlings.card.Card;
+import underlings.card.EmptyCard;
 import underlings.card.effect.domestic.ReorderTopThreeCardsEffect;
 import underlings.game.Deck;
 import underlings.gui.Gui;
@@ -39,6 +40,42 @@ public class ReorderTopThreeCardsEffectTests {
 
         EasyMock.verify(deck, gui);
         topThreeCards.forEach(EasyMock::verify);
+    }
+
+    @Test
+    public void testTwoCards() {
+        Deck deck = EasyMock.mock(Deck.class);
+        List<Card> topThreeCards = TestUtils.mockListOf(Card.class).withLength(2);
+        topThreeCards.add(EmptyCard.getInstance());
+        Gui gui = EasyMock.mock(Gui.class);
+        List<Card> reorderedCards = new ArrayList<>(topThreeCards);
+        reorderedCards.removeIf(EmptyCard.getInstance()::equals);
+        Collections.shuffle(reorderedCards);
+
+        for (Card topCard : topThreeCards) {
+            EasyMock.expect(deck.draw()).andReturn(topCard);
+        }
+        EasyMock.expect(gui.reorderCards(topThreeCards)).andReturn(reorderedCards);
+        for (Card reorderedCard : reorderedCards) {
+            deck.addCard(reorderedCard);
+        }
+
+        EasyMock.replay(deck, gui);
+        for (Card topCard : topThreeCards) {
+            if (topCard != EmptyCard.getInstance()) {
+                EasyMock.replay(topCard);
+            }
+        }
+
+        ReorderTopThreeCardsEffect testedEffect = new ReorderTopThreeCardsEffect();
+        testedEffect.on(deck).on(gui).apply();
+
+        EasyMock.verify(deck, gui);
+        for (Card topCard : topThreeCards) {
+            if (topCard != EmptyCard.getInstance()) {
+                EasyMock.verify(topCard);
+            }
+        }
     }
 
 }
