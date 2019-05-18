f7461b652fcb79e2f440a7e03b7d64f3bf6b101d
George Main IV <mainge@rose-hulman.edu>
2019-05-18 10:57:12 -0400

    refactor PointTests

--- Underlings/src/test/java/underlings/scoring/PointTests.java
+++ Underlings/src/test/java/underlings/scoring/PointTests.java
@@ -1,16 +1,12 @@
 package underlings.scoring;
 
 import static org.junit.Assert.assertEquals;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
+import underlings.Constructors;
 import underlings.card.Card;
-import underlings.gui.Gui;
-import underlings.player.Player;
 
 public class PointTests {
 
@@ -18,47 +14,26 @@ public class PointTests {
 
     @Before
     public void init() {
-        Player player = EasyMock.mock(Player.class);
-        List<Player> players = Arrays.asList(player);
-        Gui gui = EasyMock.mock(Gui.class);
-        this.scoreUtils = new ScoreUtils(players, gui);
+        this.scoreUtils = new ScoreUtils(null, null);
     }
 
     @Test
     public void testNoCards() {
         int points = this.scoreUtils.calculatePoints(Collections.emptyList());
-
         assertEquals(0, points);
     }
 
     @Test
     public void testOneCard() {
-        Card cardOne = new Card();
-        cardOne.points = 10;
-
-        List<Card> cards = new ArrayList<>();
-        cards.add(cardOne);
-
-        int points = this.scoreUtils.calculatePoints(cards);
-
+        Card[] cards = {Constructors.Card(10)};
+        int points = this.scoreUtils.calculatePoints(Arrays.asList(cards));
         assertEquals(10, points);
-
     }
 
     @Test
     public void testTwoCards() {
-        Card cardOne = new Card();
-        cardOne.points = 13;
-
-        Card cardTwo = new Card();
-        cardTwo.points = 16;
-
-        List<Card> cards = new ArrayList<>();
-        cards.add(cardOne);
-        cards.add(cardTwo);
-
-        int points = this.scoreUtils.calculatePoints(cards);
-
+        Card[] cards = {Constructors.Card(13), Constructors.Card(16)};
+        int points = this.scoreUtils.calculatePoints(Arrays.asList(cards));
         assertEquals(29, points);
     }
 
