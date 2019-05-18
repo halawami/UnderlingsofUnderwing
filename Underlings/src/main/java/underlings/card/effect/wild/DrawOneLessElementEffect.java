7660eeaeef94ecadc2fa4b4e194c3403a459da72
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-18 12:18:09 -0400

    fail testApplyTwoPlayers

---
+++ Underlings/src/main/java/underlings/card/effect/wild/DrawOneLessElementEffect.java
@@ -0,0 +1,22 @@
+package underlings.card.effect.wild;
+
+import java.util.List;
+
+import underlings.card.effect.ObserverEffect;
+import underlings.card.effect.PlayersEffect;
+import underlings.gui.Gui;
+import underlings.player.Player;
+
+public class DrawOneLessElementEffect extends PlayersEffect implements ObserverEffect {
+
+    @Override
+    protected void apply(Player currentPlayer, List<Player> players, Gui gui) {
+
+    }
+
+    @Override
+    public void onPhaseOne(Player player) {
+
+    }
+
+}
---
+++ Underlings/src/test/java/underlings/effect/players/DrawOneLessElementEffectTests.java
@@ -0,0 +1,29 @@
+package underlings.effect.players;
+
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import underlings.TestUtils;
+import underlings.card.effect.wild.DrawOneLessElementEffect;
+import underlings.player.Player;
+
+public class DrawOneLessElementEffectTests {
+
+    @Test
+    public void testApplyTwoPlayers() {
+        List<Player> players = TestUtils.mockListOf(Player.class).withLength(2);
+        DrawOneLessElementEffect testedEffect = new DrawOneLessElementEffect();
+
+        for (Player player : players) {
+            player.addObserverEffect(testedEffect);
+        }
+
+        players.forEach(EasyMock::replay);
+
+        testedEffect.on(players).apply();
+
+        players.forEach(EasyMock::verify);
+    }
+}
