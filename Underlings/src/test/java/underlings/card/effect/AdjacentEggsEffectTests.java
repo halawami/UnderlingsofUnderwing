8e9cfa3fb0ae1965dfd3f32912d05acb55c74c3c
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-19 16:04:41 -0400

    refactor AdjacentEggsEffectTests

--- Underlings/src/test/java/underlings/card/effect/AdjacentEggsEffectTests.java
+++ Underlings/src/test/java/underlings/card/effect/AdjacentEggsEffectTests.java
@@ -8,7 +8,8 @@ import org.junit.Test;
 import underlings.MockTest;
 import underlings.card.Card;
 import underlings.element.ElementBag;
-import underlings.gui.Gui;
+import underlings.handler.HandlerMovementLogic;
+import underlings.hatchingground.Deck;
 import underlings.hatchingground.HatchingGround;
 import underlings.player.Player;
 import underlings.utilities.EggHatchingUtilities;
@@ -23,23 +24,33 @@ public class AdjacentEggsEffectTests extends MockTest {
         ElementBag elementBag = this.mock(ElementBag.class);
         Player player = this.mock(Player.class);
         player.elementSpaceLogic = this.mock(ElementSpaceUtilities.class);
-        Gui gui = this.mock(Gui.class);
+        HandlerMovementLogic handlerMovementLogic = this.mock(HandlerMovementLogic.class);
         EggHatchingUtilities eggHatchingLogic = this.mock(EggHatchingUtilities.class);
+        Deck deck = this.mock(Deck.class);
         AdjacentEggsEffect testedEffect = EasyMock.partialMockBuilder(AdjacentEggsEffect.class)
                 .addMockedMethod("applyOnAdjacentEgg", Card.class, ElementSpaceUtilities.class, ElementBag.class)
-                .createNiceMock();
+                .addMockedMethod("applyOnAdjacentEgg", Card.class, EggHatchingUtilities.class)
+                .addMockedMethod("applyOnAdjacentEgg", Card.class, HandlerMovementLogic.class)
+                .addMockedMethod("applyOnAdjacentEgg", Card.class, Deck.class, HandlerMovementLogic.class,
+                        HatchingGround.class)
+                .createMock();
         this.addMock(testedEffect);
 
         List<Card> mockedCards = this.mockListOf(Card.class).withLengthOf(2);
         EasyMock.expect(hatchingGround.getAdjacentCards(centerCard)).andReturn(mockedCards);
         for (Card mockedCard : mockedCards) {
-            testedEffect.applyOnAdjacentEgg(mockedCard, null,
-                    null, hatchingGround);
+            testedEffect.applyOnAdjacentEgg(mockedCard, player.elementSpaceLogic, elementBag);
+            testedEffect.applyOnAdjacentEgg(mockedCard, eggHatchingLogic);
+            testedEffect.applyOnAdjacentEgg(mockedCard, handlerMovementLogic);
+            testedEffect.applyOnAdjacentEgg(mockedCard, deck, handlerMovementLogic, hatchingGround);
+
+
         }
 
         this.replayAll();
 
-        testedEffect.on(centerCard).on(hatchingGround).on(elementBag).on(gui).on(eggHatchingLogic).on(player).apply();
+        testedEffect.on(centerCard).on(hatchingGround).on(elementBag).on(eggHatchingLogic).on(player).on(deck)
+                .on(handlerMovementLogic).apply();
     }
 
 }
