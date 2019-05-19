6e140c67f0db4b717d0e3669ed1d1c276b137ac9
George Main IV <mainge@rose-hulman.edu>
2019-05-19 14:33:51 -0400

    fix localutilities

--- Underlings/src/main/java/underlings/card/effect/wild/adjacenteggs/ReturnAllAdjacentHandlersEffect.java
+++ Underlings/src/main/java/underlings/card/effect/wild/adjacenteggs/ReturnAllAdjacentHandlersEffect.java
@@ -5,10 +5,11 @@ import underlings.card.effect.AdjacentEggsEffect;
 import underlings.handler.HandlerChoice;
 import underlings.handler.HandlerMovementLogic;
 import underlings.player.FakePlayer;
-import underlings.utilities.LocaleWrap;
+import underlings.utilities.LocaleUtilities;
 
 public class ReturnAllAdjacentHandlersEffect extends AdjacentEggsEffect {
 
+    @Override
     public void applyOnAdjacentEgg(Card adjacentEgg, HandlerMovementLogic handlerMovementLogic) {
         handlerMovementLogic.move(adjacentEgg.handler, HandlerChoice.READY_ROOM, FakePlayer.getInstance());
         handlerMovementLogic.removeHandlerFromCard(adjacentEgg);
@@ -16,6 +17,6 @@ public class ReturnAllAdjacentHandlersEffect extends AdjacentEggsEffect {
 
     @Override
     public String toString() {
-        return LocaleWrap.get("return_adjacent_handlers");
+        return LocaleUtilities.get("return_adjacent_handlers");
     }
 }
