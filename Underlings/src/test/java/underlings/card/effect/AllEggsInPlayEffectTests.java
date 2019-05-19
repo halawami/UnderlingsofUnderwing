ab216755da588997429bb6c7ad7859873e5a8bbe
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-19 16:14:42 -0400

    fix testApplyOnCardInPlay after refactoring

--- Underlings/src/test/java/underlings/card/effect/AllEggsInPlayEffectTests.java
+++ Underlings/src/test/java/underlings/card/effect/AllEggsInPlayEffectTests.java
@@ -1,17 +1,20 @@
-package underlings.card.effect.wild.alleggsinplay;
+package underlings.card.effect;
 
 import static org.junit.Assert.assertEquals;
+
 import java.util.List;
+
 import org.easymock.EasyMock;
 import org.junit.Test;
+
 import underlings.MockTest;
 import underlings.card.Card;
-import underlings.card.effect.AllEggsInPlayEffect;
+import underlings.card.effect.wild.alleggsinplay.AddElementToAllEggsInPlayEffect;
+import underlings.card.effect.wild.alleggsinplay.DestroyAllElementsOnAllEggsInPlayEffect;
 import underlings.element.Element;
 import underlings.element.ElementBag;
 import underlings.element.ElementColor;
 import underlings.element.ElementSpace;
-import underlings.gui.Gui;
 import underlings.handler.Handler;
 import underlings.handler.HandlerMovementLogic;
 import underlings.hatchingground.HatchingGround;
@@ -52,27 +55,31 @@ public class AllEggsInPlayEffectTests extends MockTest {
         ElementSpaceUtilities elementSpaceLogic = this.mock(ElementSpaceUtilities.class);
         Card centerCard = this.mock(Card.class);
         HatchingGround hatchingGround = this.mock(HatchingGround.class);
-        Gui gui = this.mock(Gui.class);
         Player player = new Player(0, null, 0);
         player.elementSpaceLogic = elementSpaceLogic;
+        HandlerMovementLogic handlerMovementLogic = this.mock(HandlerMovementLogic.class);
         List<Card> mockedCards = this.mockListOf(Card.class).withLengthOf(numberOfCards);
         EggHatchingUtilities eggHatchingLogic = this.mock(EggHatchingUtilities.class);
         AllEggsInPlayEffect effect = EasyMock.partialMockBuilder(AllEggsInPlayEffect.class)
-                .addMockedMethod("applyOnCardInPlay", Card.class).addMockedMethod("applyOnCardInPlay", Card.class,
-                        ElementSpaceUtilities.class, ElementBag.class, HandlerMovementLogic.class)
+                .addMockedMethod("applyOnCardInPlay", Card.class)
+                .addMockedMethod("applyOnCardInPlay", Card.class, HandlerMovementLogic.class)
+                .addMockedMethod("applyOnCardInPlay", Card.class, ElementSpaceUtilities.class, ElementBag.class,
+                        HandlerMovementLogic.class)
                 .createMock();
         this.addMock(effect);
 
         EasyMock.expect(hatchingGround.getAllCards()).andReturn(mockedCards);
         for (Card mockedCard : mockedCards) {
             effect.applyOnCardInPlay(mockedCard);
-            effect.applyOnCardInPlay(mockedCard, elementSpaceLogic, elementBag, null);
+            effect.applyOnCardInPlay(mockedCard, handlerMovementLogic);
+            effect.applyOnCardInPlay(mockedCard, elementSpaceLogic, elementBag, handlerMovementLogic);
 
         }
 
         this.replayAll();
 
-        effect.on(elementBag).on(centerCard).on(hatchingGround).on(gui).on(eggHatchingLogic).on(player).apply();
+        effect.on(elementBag).on(centerCard).on(hatchingGround).on(eggHatchingLogic).on(player).on(handlerMovementLogic)
+                .apply();
     }
 
     @Test
