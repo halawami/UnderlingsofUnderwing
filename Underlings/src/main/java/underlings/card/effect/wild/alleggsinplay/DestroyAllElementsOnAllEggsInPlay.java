50359523b748f0d018706d409bf2c91aff935901
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-19 14:43:07 -0400

    refactor DestroyAllElementsOnAllEggsInPlay to use applyOnCardInPlay(Card cardInPlay)

--- Underlings/src/main/java/underlings/card/effect/wild/alleggsinplay/DestroyAllElementsOnAllEggsInPlay.java
+++ Underlings/src/main/java/underlings/card/effect/wild/alleggsinplay/DestroyAllElementsOnAllEggsInPlay.java
@@ -1,16 +1,12 @@
 package underlings.card.effect.wild.alleggsinplay;
 
 import underlings.card.Card;
-import underlings.element.ElementBag;
-import underlings.handler.HandlerMovementLogic;
-import underlings.utilities.ElementSpaceUtilities;
 import underlings.utilities.LocaleUtilities;
 
 public class DestroyAllElementsOnAllEggsInPlay extends AllEggsInPlayEffect {
 
     @Override
-    public void applyOnCardInPlay(Card cardInPlay, ElementSpaceUtilities elementSpaceLogic, ElementBag elementBag,
-            HandlerMovementLogic handlerMovementLogic) {
+    public void applyOnCardInPlay(Card cardInPlay) {
         if (cardInPlay.isClaimed()) {
             for (int i = 0; i < cardInPlay.elementSpaces.length; i++) {
                 cardInPlay.elementSpaces[i].destroyAllElements();
