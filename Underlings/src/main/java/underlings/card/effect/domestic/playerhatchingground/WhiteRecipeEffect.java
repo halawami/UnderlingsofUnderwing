39b5cd1c594302657cd3d22ffdf67aa59f64b310
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-19 13:15:06 -0400

    refactor WhiteRecipeEffect to extend PlayerHatchingGroundEffect

--- Underlings/src/main/java/underlings/card/effect/domestic/playerhatchingground/WhiteRecipeEffect.java
+++ Underlings/src/main/java/underlings/card/effect/domestic/playerhatchingground/WhiteRecipeEffect.java
@@ -3,43 +3,35 @@ package underlings.card.effect.domestic.playerhatchingground;
 import java.util.ArrayList;
 import java.util.List;
 
-import underlings.card.Card;
-import underlings.card.effect.DiverseHatchingGroundEffect;
-import underlings.element.ElementBag;
 import underlings.element.ElementColor;
-import underlings.game.Deck;
 import underlings.game.HatchingGround;
 import underlings.gui.Gui;
-import underlings.handler.HandlerMovementLogic;
 import underlings.player.Player;
 import underlings.utilities.EggHatchingLogic;
 import underlings.utilities.LocaleWrap;
 
-public class WhiteRecipeEffect extends DiverseHatchingGroundEffect {
+public class WhiteRecipeEffect extends PlayerHatchingGroundEffect {
 
-    String recipe000 = "RED,YELLOW,BLUE,PURPLE,ORANGE,GREEN";
-    String recipe001 = "RED,YELLOW,BLUE,PURPLE,ORANGE,BLUE,YELLOW";
-    String recipe010 = "RED,YELLOW,BLUE,PURPLE,RED,YELLOW,GREEN";
-    String recipe011 = "RED,YELLOW,BLUE,PURPLE,RED,YELLOW,BLUE,YELLOW";
-    String recipe100 = "RED,YELLOW,BLUE,RED,BLUE,ORANGE,GREEN";
-    String recipe101 = "RED,YELLOW,BLUE,RED,BLUE,ORANGE,BLUE,YELLOW";
-    String recipe110 = "RED,YELLOW,BLUE,RED,BLUE,RED,YELLOW,GREEN";
-    String recipe111 = "RED,YELLOW,BLUE,RED,BLUE,RED,YELLOW,BLUE,YELLOW";
+    private String recipe000 = "RED,YELLOW,BLUE,PURPLE,ORANGE,GREEN";
+    private String recipe001 = "RED,YELLOW,BLUE,PURPLE,ORANGE,BLUE,YELLOW";
+    private String recipe010 = "RED,YELLOW,BLUE,PURPLE,RED,YELLOW,GREEN";
+    private String recipe011 = "RED,YELLOW,BLUE,PURPLE,RED,YELLOW,BLUE,YELLOW";
+    private String recipe100 = "RED,YELLOW,BLUE,RED,BLUE,ORANGE,GREEN";
+    private String recipe101 = "RED,YELLOW,BLUE,RED,BLUE,ORANGE,BLUE,YELLOW";
+    private String recipe110 = "RED,YELLOW,BLUE,RED,BLUE,RED,YELLOW,GREEN";
+    private String recipe111 = "RED,YELLOW,BLUE,RED,BLUE,RED,YELLOW,BLUE,YELLOW";
 
     @Override
-    protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag, Gui gui,
-            Player currentPlayer, EggHatchingLogic eggHatchingLogic, Deck deck,
-            HandlerMovementLogic handlerMovementLogic) {
-
+    protected void apply(HatchingGround hatchingGround, EggHatchingLogic hatchingLogic, Player currentPlayer, Gui gui) {
         List<String[]> recipes = new ArrayList<>();
-        recipes.add(recipe000.split(","));
-        recipes.add(recipe001.split(","));
-        recipes.add(recipe010.split(","));
-        recipes.add(recipe011.split(","));
-        recipes.add(recipe100.split(","));
-        recipes.add(recipe101.split(","));
-        recipes.add(recipe110.split(","));
-        recipes.add(recipe111.split(","));
+        recipes.add(this.recipe000.split(","));
+        recipes.add(this.recipe001.split(","));
+        recipes.add(this.recipe010.split(","));
+        recipes.add(this.recipe011.split(","));
+        recipes.add(this.recipe100.split(","));
+        recipes.add(this.recipe101.split(","));
+        recipes.add(this.recipe110.split(","));
+        recipes.add(this.recipe111.split(","));
 
         for (String[] recipe : recipes) {
             currentPlayer.elementSpaceLogic.addRecipe(ElementColor.WHITE, recipe);
@@ -51,4 +43,6 @@ public class WhiteRecipeEffect extends DiverseHatchingGroundEffect {
     public String toString() {
         return LocaleWrap.get("combine_primary_secondary_for_white");
     }
+
+
 }
