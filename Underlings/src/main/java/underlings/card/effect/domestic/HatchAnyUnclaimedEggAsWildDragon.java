4e6951f6eafb86c8957ad8370474f0b23dbcceda
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-19 13:05:29 -0400

    refactor HatchAnyUnclaimedEggAsWildDragon to extend PlayerHatchingGroundEffect

--- Underlings/src/main/java/underlings/card/effect/domestic/HatchAnyUnclaimedEggAsWildDragon.java
+++ Underlings/src/main/java/underlings/card/effect/domestic/HatchAnyUnclaimedEggAsWildDragon.java
@@ -3,41 +3,35 @@ package underlings.card.effect.domestic;
 import java.util.List;
 
 import underlings.card.Card;
-import underlings.card.effect.DiverseHatchingGroundEffect;
-import underlings.element.ElementBag;
-import underlings.game.Deck;
 import underlings.game.HatchingGround;
 import underlings.gui.Gui;
 import underlings.gui.YesNoChoice;
-import underlings.handler.HandlerMovementLogic;
 import underlings.player.FakePlayer;
 import underlings.player.Player;
 import underlings.utilities.EggHatchingLogic;
 import underlings.utilities.LocaleWrap;
 
-public class HatchAnyUnclaimedEggAsWildDragon extends DiverseHatchingGroundEffect {
-
-    @Override
-    protected void apply(Card centerCard, HatchingGround hatchingGround, ElementBag elementBag, Gui gui,
-            Player currentPlayer, EggHatchingLogic eggHatchingLogic, Deck deck,
-            HandlerMovementLogic handlerMovementLogic) {
-        YesNoChoice choice = gui.promptChoice(LocaleWrap.get("prompt_choice_hatch_wildly"), YesNoChoice.getChoices(),
-                currentPlayer.getId());
-        if (choice == YesNoChoice.YES) {
-            List<Card> unclaimedEggs = hatchingGround.getUnclaimedEggs();
-            if (unclaimedEggs.size() == 0) {
-                gui.notifyAction(currentPlayer.getId(), LocaleWrap.get("notify_no_unclaimed_eggs"));
-            } else {
-                Card toHatch =
-                        gui.promptChoice(LocaleWrap.get("prompt_card_hatch_wildly"), unclaimedEggs, currentPlayer.id);
-                eggHatchingLogic.hatchEgg(toHatch, FakePlayer.getInstance());
-            }
-        }
-    }
+public class HatchAnyUnclaimedEggAsWildDragon extends PlayerHatchingGroundEffect {
 
     @Override
     public String toString() {
         return LocaleWrap.get("hatch_egg_as_wild_dragon_effect");
     }
 
+    @Override
+    protected void apply(HatchingGround hatchingGround, EggHatchingLogic hatchingLogic, Player currentPlayer, Gui gui) {
+        YesNoChoice choice = gui.promptChoice(LocaleWrap.get("prompt_choice_hatch_wildly"), YesNoChoice.getChoices(),
+                currentPlayer.getId());
+
+        if (choice == YesNoChoice.YES) {
+            List<Card> unclaimedEggs = hatchingGround.getUnclaimedEggs();
+            if (unclaimedEggs.size() == 0) {
+                gui.notifyAction(currentPlayer.getId(), LocaleWrap.get("notify_no_unclaimed_eggs"));
+            } else {
+                Card toHatch = gui
+                        .promptChoice(LocaleWrap.get("prompt_card_hatch_wildly"), unclaimedEggs, currentPlayer.id);
+                hatchingLogic.hatchEgg(toHatch, FakePlayer.getInstance());
+            }
+        }
+    }
 }
