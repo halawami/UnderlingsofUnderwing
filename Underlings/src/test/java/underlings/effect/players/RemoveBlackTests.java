04c01564548539a9283881d9251498c1d47fe1de
Mohammad Baqer <mtbaqer@gmail.com>
2019-05-19 04:11:06 -0400

    refactor RemoveBlackTests to use MockTest

--- Underlings/src/test/java/underlings/effect/players/RemoveBlackTests.java
+++ Underlings/src/test/java/underlings/effect/players/RemoveBlackTests.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 
-import org.easymock.EasyMock;
 import org.junit.Test;
 
+import underlings.MockTest;
 import underlings.card.effect.Effect;
 import underlings.card.effect.wild.RemoveBlackRecipesEffect;
 import underlings.element.ElementColor;
@@ -15,38 +15,34 @@ import underlings.element.utilities.ElementSpaceLogic;
 import underlings.player.Player;
 import underlings.utilities.LocaleWrap;
 
-public class RemoveBlackTests {
+public class RemoveBlackTests extends MockTest {
 
     @Test
     public void testEffect() {
-        ElementSpaceLogic logic1 = EasyMock.mock(ElementSpaceLogic.class);
+        ElementSpaceLogic logic1 = this.mock(ElementSpaceLogic.class);
         logic1.resetRecipes(ElementColor.BLACK);
-        Player player1 = EasyMock.mock(Player.class);
+        Player player1 = this.mock(Player.class);
         player1.elementSpaceLogic = logic1;
 
-        ElementSpaceLogic logic2 = EasyMock.mock(ElementSpaceLogic.class);
+        ElementSpaceLogic logic2 = this.mock(ElementSpaceLogic.class);
         logic2.resetRecipes(ElementColor.BLACK);
-        Player player2 = EasyMock.mock(Player.class);
+        Player player2 = this.mock(Player.class);
         player2.elementSpaceLogic = logic2;
 
-        ElementSpaceLogic logic3 = EasyMock.mock(ElementSpaceLogic.class);
+        ElementSpaceLogic logic3 = this.mock(ElementSpaceLogic.class);
         logic3.resetRecipes(ElementColor.BLACK);
-        Player player3 = EasyMock.mock(Player.class);
+        Player player3 = this.mock(Player.class);
         player3.elementSpaceLogic = logic3;
 
-        ElementSpaceLogic logic4 = EasyMock.mock(ElementSpaceLogic.class);
+        ElementSpaceLogic logic4 = this.mock(ElementSpaceLogic.class);
         logic4.resetRecipes(ElementColor.BLACK);
-        Player player4 = EasyMock.mock(Player.class);
+        Player player4 = this.mock(Player.class);
         player4.elementSpaceLogic = logic4;
 
-        EasyMock.replay(player1, player2, player3, player4);
-        EasyMock.replay(logic1, logic2, logic3, logic4);
+        this.replayAll();
 
         List<Player> players = Arrays.asList(player1, player2, player3, player4);
         new RemoveBlackRecipesEffect().on(players).apply();
-
-        EasyMock.verify(player1, player2, player3, player4);
-        EasyMock.verify(logic1, logic2, logic3, logic4);
     }
 
     @Test
